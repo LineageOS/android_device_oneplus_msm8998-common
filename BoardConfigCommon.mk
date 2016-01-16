694d220dcc6263c7830c4b5b6ab7c6b34f3b05fb
xboxfanj <xboxlover360@gmail.com>
2016-01-16 18:45:48 +0100

    msm8998-common: Clean up kernel cmdline
     * CONFIG_DEBUG_USER is disabled, so no need to keep user_debug
    
    Change-Id: I9a30f32f7dc91cf82f2516bf6a54d7d62f1c00f1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,7 @@ BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
