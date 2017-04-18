1d7f6358ff608b1bc59f67ff24563e583c85fdab
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    msm8998-common: Enforce vendor tree presence
    A compiled build has no chance of booting without the prebuilt
    binaries, so fail the build if those are not present.
    
    Change-Id: I8124a5f92a75490ce48422b9d760764733efe37e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,4 +155,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
--include vendor/oneplus/msm8998-common/BoardConfigVendor.mk
+include vendor/oneplus/msm8998-common/BoardConfigVendor.mk
