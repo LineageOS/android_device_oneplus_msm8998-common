3ece17468b2205f553a0385a8c1a1d5d7bf469d9
myfluxi <linflux@arcor.de>
2017-08-22 16:22:25 +0200

    msm8998: add pocketmode app
    *) Most of the original commit was still here.  This is just
       adding back a couple of missing pieces.
    
    Change-Id: I58b550dad0a71f3d9bd9865c00161f952512c898

--- common.mk
+++ common.mk
@@ -190,7 +190,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    fingerprintd
+    fingerprintd \
+    OneplusPocketMode
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
