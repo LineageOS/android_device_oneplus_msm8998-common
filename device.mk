380fb16f4a39b1cbe147ee02e35e6bc84d3eae2f
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-09-03 19:59:55 +0200

    cheeseburger: media: update profiles, drop unsupported profiles
    Change-Id: I33e1b8b5ff202a5b102d1492d068c26b9251510d

--- device.mk
+++ device.mk
@@ -265,7 +265,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
