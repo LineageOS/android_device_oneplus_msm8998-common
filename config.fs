0aed2df3b9e22135b6411897b32477f3e8b78cb8
Alexander Martinz <alex@amartinz.at>
2017-09-10 20:57:14 +0200

    cheeseburger: config.fs: add vendor entries
    Change-Id: I66ef4944575b8e693308f315c0ec1ba38d08e7b6
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- config.fs
+++ config.fs
@@ -78,3 +78,39 @@ mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: SYS_NICE
+
+[vendor/bin/cnd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND NET_ADMIN
+
+[vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
+[vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[vendor/bin/pm-service]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[vendor/bin/slim_daemon]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE
+
+[vendor/bin/wcnss_filter]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND
