f19e2b997500d1ac163223e3c697008bd5fec252
Alexander Martinz <alex@amartinz.at>
2017-09-10 20:24:00 +0200

    cheeseburger: binderize all the things!
    -----
    
    cheeseburger: add omx to manifest
    
    -impl does not exist anymore
    
    -----
    
    cheeseburger: add audio to manifest, build service
    
    -----
    
    cheeseburger: add drm to manifest, build impl
    
    -----
    
    cheeseburger: add broadcastradio to manifest, build impl
    
    -----
    
    cheeseburger: add thermal to manifest, build impl
    
    -----
    
    cheeseburger: omx: build libhypv_intercept
    
    -----
    
    Change-Id: I41d363de67e7fc98d034dcccb5fd64addb42b3ab
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- device.mk
+++ device.mk
@@ -109,6 +109,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     audio.a2dp.default \
     audio.primary.msm8998 \
@@ -159,7 +160,8 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
-    libbt-vendor
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor \
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -176,8 +178,11 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.configstore@1.0-service \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     copybit.msm8998 \
     gralloc.msm8998 \
     hwcomposer.msm8998 \
@@ -195,7 +200,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-service.widevine \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
@@ -224,6 +231,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/lowi.conf:system/vendor/etc/lowi.conf \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/vendor/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/vendor/etc/xtwifi.conf
+
+# Healthd
+#PRODUCT_PACKAGES += \
+#    android.hardware.health@1.0-convert \
+#    android.hardware.health@1.0-impl \
+#    android.hardware.health@1.0-service \
 
 # IPv6
 PRODUCT_PACKAGES += \
@@ -290,6 +303,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libextmedia_jni \
+    libhypv_intercept \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
@@ -363,6 +377,10 @@ PRODUCT_PACKAGES += \
 # Shims
 PRODUCT_PACKAGES += \
     libshims_system_properties
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
 
 # USB
 PRODUCT_PACKAGES += \
