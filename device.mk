74fce7609af7459608ae649c3e1765683a3b18f8
Alexander Martinz <alex@amartinz.at>
2017-09-13 00:01:51 +0200

    cheeseburger: do not manually copy manifest.xml
    Use the DEVICE_MANIFEST_FILE way, which we have set already
    
    Change-Id: I672e7b8977841e892babc74a55efec8bb4bd9edf
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- device.mk
+++ device.mk
@@ -225,10 +225,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/vendor/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/vendor/etc/xtwifi.conf
 
-# HIDL
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
-
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
