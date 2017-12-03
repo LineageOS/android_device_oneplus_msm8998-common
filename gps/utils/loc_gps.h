1a29dcf42f0f78ee3c85b1731966ed1670ab9b08
Hamster Tian <haotia@gmail.com>
2017-12-03 22:18:35 +0800

    msm8998: update GPS HAL to LA.UM.6.4.r1-04900
    Change-Id: I3e4ff4669b9478b38c83cf06fcb66ec795c131e1

--- gps/utils/loc_gps.h
+++ gps/utils/loc_gps.h
@@ -98,6 +98,8 @@ typedef uint16_t LocGpsLocationFlags;
 #define LOC_GPS_LOCATION_HAS_BEARING    0x0008
 /** LocGpsLocation has valid accuracy. */
 #define LOC_GPS_LOCATION_HAS_ACCURACY   0x0010
+/** LocGpsLocation has valid vertical uncertainity */
+#define LOC_GPS_LOCATION_HAS_VERT_UNCERTAINITY   0x0040
 
 /** Flags for the loc_gps_set_capabilities callback. */
 
@@ -206,7 +208,7 @@ typedef int LocGpsNiEncodingType;
 #define LOC_GPS_ENC_UNKNOWN                -1
 
 /** AGPS status event values. */
-typedef uint16_t LocAGpsStatusValue;
+typedef uint8_t LocAGpsStatusValue;
 /** GPS requests data connection for AGPS. */
 #define LOC_GPS_REQUEST_AGPS_DATA_CONN  1
 /** GPS releases the AGPS data connection. */
@@ -539,12 +541,14 @@ typedef struct {
      * Represents altitude in meters above the WGS 84 reference ellipsoid.
      */
     double          altitude;
-    /** Represents speed in meters per second. */
+    /** Represents horizontal speed in meters per second. */
     float           speed;
     /** Represents heading in degrees. */
     float           bearing;
     /** Represents expected accuracy in meters. */
     float           accuracy;
+    /** Represents the expected vertical uncertainity in meters*/
+    float           vertUncertainity;
     /** Timestamp for the location fix. */
     LocGpsUtcTime      timestamp;
 } LocGpsLocation;
