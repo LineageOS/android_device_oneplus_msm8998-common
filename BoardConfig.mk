92ae8b24c8e1e5aab17ab808c53a1affc7e93e4b
Alexander Martinz <alex@amartinz.at>
2017-12-04 23:04:01 +0100

    cheeseburger: use oneplus5 defconfig
    Change-Id: I950eb1e6087336220678123c482244531b74b9d2
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,7 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
-TARGET_KERNEL_CONFIG := lineage_cheeseburger_defconfig
+TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # REMOVE ASAP
