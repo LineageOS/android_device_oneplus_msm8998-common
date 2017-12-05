c2afff920b54dc79e184c47ee66441f74f0f07df
Alexander Martinz <alex@amartinz.at>
2017-12-05 15:19:25 +0100

    msm8998-common: move fingerprint packages to device tree
    Change-Id: I2b1bad733bb2a4bd9ceb7cb07f6c62b7ab5ba749
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- common.mk
+++ common.mk
@@ -183,11 +183,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.0-service.widevine \
 
-# Fingerprint sensor
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
-    fingerprintd \
-
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += \
     fs_config_files
