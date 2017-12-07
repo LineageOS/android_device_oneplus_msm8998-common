01897fb372c0e8426ad95ba8dedac487de92d2a6
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 15:15:00 +0100

    msm8998: Build a dummy android.hidl.base@1.0 for Oreo-MR0 blobs
    Change-Id: I50d02364368691da2bc081988d500587cc427852

--- common.mk
+++ common.mk
@@ -214,6 +214,11 @@ PRODUCT_COPY_FILES += \
 #    android.hardware.health@1.0-impl \
 #    android.hardware.health@1.0-service \
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0-java
+
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
