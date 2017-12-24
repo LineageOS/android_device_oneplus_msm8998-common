271d8677483bb74d1d3b597da0fbbc90322db8d9
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-12-24 13:54:48 +0100

    msm8998: update blobs from Oxygen 5.0
    Change-Id: I30b2a2d955aed82505b5b966cdf62e2858938aac

--- init/vendor/bin/init.qti.qseecomd.sh
+++ init/vendor/bin/init.qti.qseecomd.sh
@@ -1,5 +1,5 @@
-#!/system/bin/sh
-# Copyright (c) 2010, The Linux Foundation. All rights reserved.
+#!/vendor/bin/sh
+# Copyright (c) 2016,2017 The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -12,7 +12,7 @@
 #       with the distribution.
 #     * Neither the name of The Linux Foundation nor the names of its
 #       contributors may be used to endorse or promote products derived
-#      from this software without specific prior written permission.
+#       from this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
 # WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
@@ -26,52 +26,10 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
+#
 
-# For successful WLAN card detection, WLAN needs SDIO polling turned on.
-# This script can be used to turn on/off SDIO polling on appropriate
-# SDIO slot on the MSM target (e.g. slot 3 on 7x30 surf).
-
-arg=$1
-target=`getprop ro.board.platform`
-
-case "$target" in
-    "msm7627_6x")
-        echo 1 > /sys/devices/platform/msm_sdcc.1/polling
-        echo 1 > /sys/devices/platform/msm_sdcc.2/polling
-        ;;
-
-    "msm7627_ffa")
-        echo 1 > /sys/devices/platform/msm_sdcc.2/polling
-        ;;
-
-    "msm7627_surf")
-        echo 1 > /sys/devices/platform/msm_sdcc.1/polling
-        echo 1 > /sys/devices/platform/msm_sdcc.2/polling
-        ;;
-
-    "msm7627a")
-        echo 1 > /sys/devices/platform/msm_sdcc.2/polling
-        ;;
-
-    "msm7630_surf")
-        echo 1 > /sys/devices/platform/msm_sdcc.3/polling
-        ;;
-
-    "msm7630_1x")
-        echo 1 > /sys/devices/platform/msm_sdcc.3/polling
-        ;;
-
-    "msm7630_fusion")
-        echo 1 > /sys/devices/platform/msm_sdcc.3/polling
-        ;;
-
-    "msm8660")
-        echo 1 > /sys/devices/platform/msm_sdcc.4/polling
-        ;;
-
-    "msm8660_csfb")
-        echo 1 > /sys/devices/platform/msm_sdcc.4/polling
-        ;;
-esac
-
-exit 0
+while [ "$registered" != "true" ]
+do
+    sleep 0.1
+    registered="`getprop sys.listeners.registered`"
+done
