d14723b990f65760c8748c30d54287b349db1afe
jumoog <kilianvonpflugk@gmail.com>
2017-12-24 23:35:42 +0000

    msm8998: re-enable Healthd
    Change-Id: Ie69182783d071ef0130d812a3cc1146c2d833abf

--- common.mk
+++ common.mk
@@ -209,10 +209,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/vendor/etc/xtwifi.conf
 
 # Healthd
-#PRODUCT_PACKAGES += \
-#    android.hardware.health@1.0-convert \
-#    android.hardware.health@1.0-impl \
-#    android.hardware.health@1.0-service \
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-convert \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-service \
 
 # HIDL
 PRODUCT_PACKAGES += \
