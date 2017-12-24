7c4981b2f4c1f86db73af5de6cf751bcece55c43
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-12-24 13:54:48 +0100

    msm8998: update blobs from Oxygen 5.0
    Change-Id: I30b2a2d955aed82505b5b966cdf62e2858938aac

--- init/vendor/bin/init.mdm.sh
+++ init/vendor/bin/init.mdm.sh
@@ -1,5 +1,6 @@
-#!/system/bin/sh
-# Copyright (c) 2015, The Linux Foundation. All rights reserved.
+#! /vendor/bin/sh
+
+# Copyright (c) 2013, The Linux Foundation. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -8,7 +9,7 @@
 #     * Redistributions in binary form must reproduce the above copyright
 #       notice, this list of conditions and the following disclaimer in the
 #       documentation and/or other materials provided with the distribution.
-#     * Neither the name of The Linux Foundation nor
+#     * Neither the name of Linux Foundation nor
 #       the names of its contributors may be used to endorse or promote
 #       products derived from this software without specific prior written
 #       permission.
@@ -26,25 +27,8 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-#
-# Function to start sensors for SSC enabled platforms
-#
-start_sensors()
-{
-    if [ -c /dev/msm_dsps -o -c /dev/sensors ]; then
-        chmod -h 775 /persist/sensors
-        chmod -h 664 /persist/sensors/sensors_settings
-        chown -h system.root /persist/sensors/sensors_settings
-        # ifdef VENDOR_EDIT
-        #qiuchangping@BSP 2015-11-24 add for gyro sensitity calibration
-        chmod -h 664 /persist/sensors/gyro_sensitity_cal
-        chown -h system.root /persist/sensors/gyro_sensitity_cal
-        # endif
-        mkdir -p /data/misc/sensors
-        chmod -h 775 /data/misc/sensors
+baseband=`getprop ro.baseband`
+if [ "$baseband" = "mdm" ] || [ "$baseband" = "mdm2" ]; then
+	start mdm_helper
+fi
 
-        start sensors
-    fi
-}
-
-start_sensors
