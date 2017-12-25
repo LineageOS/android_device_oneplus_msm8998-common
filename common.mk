4052392982af4c77427535770f6f4b5a37dc7d3b
jumoog <kilianvonpflugk@gmail.com>
2017-12-25 10:18:04 +0000

    msm8998: remove unneeded libshims
    Change-Id: If369610a0b75d37a439e6f30f4679a658d956e7e

--- common.mk
+++ common.mk
@@ -360,10 +360,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     sensors.msm8998
 
-# Shims
-PRODUCT_PACKAGES += \
-    libshims_system_properties
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
