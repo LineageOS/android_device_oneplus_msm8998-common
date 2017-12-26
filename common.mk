02636285e3b648a253386f1c2432d9dba99e36f5
jumoog <kilianvonpflugk@gmail.com>
2017-12-26 12:24:14 +0000

    keymaster haxxx
    Change-Id: I24061671db3dd8c80e822adb128521b07c6baf83

--- common.mk
+++ common.mk
@@ -235,7 +235,10 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
