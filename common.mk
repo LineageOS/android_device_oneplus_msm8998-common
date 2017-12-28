d3ef0aea3681e8fc07f3b41d349c83d59bb4c2b1
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-12-28 19:53:42 +0100

    msm8998: add additional qcom/qti services and blobs from OOS 5.0
    Change-Id: I544cfeebd790016a31e564edf0111e340e2af21b

--- common.mk
+++ common.mk
@@ -136,7 +136,6 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
     Camera2
 
