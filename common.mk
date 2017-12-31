888e44fa3a555da341f538e94d9995a6c98b2313
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-12-31 14:40:17 +0100

    msm8998: build qti power hal
    Change-Id: Ifdb9a343ab4c7cc5c2375e346034660600ea3501

--- common.mk
+++ common.mk
@@ -322,9 +322,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.msm8998
+    android.hardware.power@1.1-service-qti
 
 # QMI
 PRODUCT_PACKAGES += \
