ab0eb8b59e0b2f01b7b49a43b0684f84623d437e
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-01 16:51:05 +0100

    msm8998: selinux: rework policies and enforce
    Change-Id: If4cf07a59f0c2fd7c3480b14318653c5414a96b4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,9 +79,6 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
-# REMOVE ASAP
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
 
@@ -268,7 +265,7 @@ PROTOBUF_SUPPORTED := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
