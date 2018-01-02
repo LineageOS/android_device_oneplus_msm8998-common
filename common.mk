bf30dcf071a1053d4be02f45a265aea1b17f7fa4
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-02 20:38:25 +0100

    msm8998: import ipa from LA.UM.6.4.r1-05700-8x98.0 and enable tetheroffload
    Change-Id: I927c583502a3636524815dcfdcc150c98be460dd

--- common.mk
+++ common.mk
@@ -357,6 +357,11 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+# Tetheroffload
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
