0ec40503f01181e1d657cd6f39bef44af184d9f1
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-02 11:41:39 +0100

    msm8998: remove ipacm/ipanat
    Dropped with O
    
    Change-Id: I0439ef92b89e5a1b2f387852cf182cba131b8433

--- common.mk
+++ common.mk
@@ -380,9 +380,6 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    ipacm \
-    ipacm-diag \
-    IPACM_cfg.xml \
     libqsap_sdk \
     libQWiFiSoftApCfg \
     libwpa_client \
