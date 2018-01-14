b0265e1314e02486d780cd7b9f0bf6d463e67837
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-14 13:18:31 +0100

    msm8998: add 15s wifi driver probe delay
    Needs https://review.lineageos.org/#/c/203258/
    
    Change-Id: Iccac9aaa67a7b543e2f79af50f13fd566f79a7a7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -283,6 +283,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_PROBE_DELAY := 15
 
 # inherit from the proprietary version
 -include vendor/oneplus/msm8998-common/BoardConfigVendor.mk
