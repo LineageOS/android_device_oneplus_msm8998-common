793c7d82b1cf992c7316fd742db33ef5de20acf8
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-28 12:23:12 +0100

    msm8998: add vndk sp libs
    Change-Id: If6eeb32084e72cb0d773b666921d6de7c9187392

--- common.mk
+++ common.mk
@@ -223,7 +223,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0-convert \
     android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service \
+    android.hardware.health@1.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
@@ -389,6 +389,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
+
+# VNDK-SP:
+PRODUCT_PACKAGES += \
+    vndk-sp
 
 # VR
 PRODUCT_PACKAGES += \
