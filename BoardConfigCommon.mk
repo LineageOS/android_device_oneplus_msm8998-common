5a31bdd25369b42d648f8c58cd549cdf2c3b2eba
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-03 22:12:59 +0100

    msm8998: wifi: wait for driver to get ready
    Change-Id: Ibea8a5f6e3c1ec43e721c615b2ecccf9a1fab644

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -283,6 +283,8 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_STATE_PATH := "/sys/kernel/wlan_state/probe"
+WIFI_DRIVER_STATE_ON := "1"
 
 # inherit from the proprietary version
 -include vendor/oneplus/msm8998-common/BoardConfigVendor.mk
