ade118dc71d7ed1b11b7ec758510c1fc30b58d46
XiNGRZ <chenxingyu92@gmail.com>
2018-02-05 23:41:55 +0800

    msm8998: Go treble!
    Change-Id: I174f2c7b6af2bf1b3e4abce6ce035961d098c9e3

--- common.mk
+++ common.mk
@@ -80,10 +80,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Device was launched with N
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.product.first_api_level=25
-
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard
 
