4838f3ae45723d1e83fbee20c701d2863a9b221e
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-17 21:11:27 +0100

    msm8998: wifi: wait for kernel driver to get ready
    Change-Id: Ieced27fb0bb5cba6d9850de594b5d4df7977991d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -284,6 +284,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
 
 # inherit from the proprietary version
 -include vendor/oneplus/msm8998-common/BoardConfigVendor.mk
