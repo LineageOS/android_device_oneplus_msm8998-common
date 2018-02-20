a3d39359bd7c6fd0a8e79e71ee2c547e98d570fd
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-20 20:53:37 +0100

    msm8998: wifi: load driver on late initcall
    https://review.lineageos.org/#/c/207253/
    
    Change-Id: Ie593195c6ee7095501755859abc41aea5470a7d7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -284,9 +284,6 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
-WIFI_DRIVER_STATE_CTRL_PARAM := "/sys/kernel/boot_wlan/boot_wlan"
-WIFI_DRIVER_STATE_ON := 1
-WIFI_DRIVER_STATE_OFF := 0
 
 # inherit from the proprietary version
 -include vendor/oneplus/msm8998-common/BoardConfigVendor.mk
