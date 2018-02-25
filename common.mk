d7e54bdbf0e1e1a95464a627b420ca83077849ca
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-25 08:12:00 +0100

    msm8998: include dalvik and hwui prop overrides from vendor/lineage
    Change-Id: I1e550ff4f814d10a0a516cf791b026f806d742f3

--- common.mk
+++ common.mk
@@ -27,28 +27,8 @@ $(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapmaxfree=8m
-
-# HWUI
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=96 \
-    ro.hwui.layer_cache_size=64 \
-    ro.hwui.r_buffer_cache_size=12 \
-    ro.hwui.path_cache_size=39 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=7 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.text_small_cache_width=2048 \
-    ro.hwui.text_small_cache_height=2048 \
-    ro.hwui.text_large_cache_width=3072 \
-    ro.hwui.text_large_cache_height=4096
+$(call inherit-product, vendor/lineage/build/memory/phone-xxhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, vendor/lineage/build/memory/phone-xxhdpi-6144-hwui-memory.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
