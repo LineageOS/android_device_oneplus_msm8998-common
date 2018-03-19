6af2b6465544815830f662b0d49d8127a6524030
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-19 21:58:37 +0100

    oneplus-msm8998: Build hexagon neuralnetworks HAL and add controller
    Change-Id: If0c38b07e4b62bac201cf756ec779e3e8717d302

--- common.mk
+++ common.mk
@@ -321,6 +321,10 @@ PRODUCT_PACKAGES += \
     libandroid_net \
     netutils-wrapper-1.0
 
+# Neural Networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.0-service-hvx
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
