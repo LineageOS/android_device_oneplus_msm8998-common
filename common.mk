4cbbb74747306e0efef7ba703ee865d648f4c663
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-24 11:26:10 -0700

    msm8998: Enable TARGET_USES_QTI_CAMERA_DEVICE
    Change-Id: I9b80b810ea43b2743557b6368d6113f3065da029

--- common.mk
+++ common.mk
@@ -160,6 +160,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service.oneplus5 \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     Snap
 
