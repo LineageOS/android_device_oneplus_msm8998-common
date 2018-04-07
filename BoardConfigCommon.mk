d2bb58431e451aaab12da6b72e90d5c9ab0ec9f4
XiNGRZ <chenxingyu92@gmail.com>
2018-04-07 21:03:18 +0800

    msm8998: Split system and vendor properties
    Change-Id: I863e71534dd6b1d56a79bcfbe48a667807e6e9c1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -250,6 +250,10 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_USES_INTERACTION_BOOST := true
 
+# Properties
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
+TARGET_VENDOR_PROP += $(PLATFORM_PATH)/vendor.prop
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/recovery.fstab
 #TARGET_RECOVERY_UI_LIB := librecovery_ui_msm
