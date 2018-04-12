18373f1dbe7ab0bed06d2228582908fea48efb4b
Vitalii Kulikov <solk@solk.org.ua>
2018-04-12 20:53:06 +0200

    msm8998: Remove camera provider HAL fork
    No longer needed with 8.1 blobs and fixes face detection
    
    Change-Id: Ia34c4e4686476c6e2a2264f6521d288b4ceb1c16

--- common.mk
+++ common.mk
@@ -161,7 +161,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service.oneplus5 \
+    android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     Snap
