4df21543f8e228e699fba80b991114688b854438
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-04-29 10:13:48 +0200

    msm8998: gps: add libgnsspps sources from caf
    https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/location/log/?h=LA.UM.6.4.r1-07600-8x98.0
    
    Change-Id: I206d9d16db6d211e6a45b147a95302c088b98d2a

--- common.mk
+++ common.mk
@@ -227,11 +227,12 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
-# GPS
+# GPS / Location
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
     android.hardware.gnss@1.0-service-qti \
     libgnss \
+    libgnsspps \
     libgps.utils \
     liblocation_api \
     libloc_core \
