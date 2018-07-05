51f172c1a7f18f69a1d55605f7b6c8643dea5206
Michael Bestas <mkbestas@lineageos.org>
2018-07-05 19:27:45 +0300

    msm8998-common: sed qti_libpermissions.xml during extract instead of pinning
    Change-Id: I9687d74892659a1e332d6842c8f6a2184f55a7ca
    (cherry picked from commit 436bc2336bd4fc9cc2788451cbfdd123c90e903c)

--- extract-files.sh
+++ extract-files.sh
@@ -66,10 +66,12 @@ if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
     extract "$MY_DIR"/../$DEVICE/proprietary-files.txt "$SRC" "$SECTION"
 fi
 
+COMMON_BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
+
 function fix_camera_etc_path () {
     sed -i \
         's/\/system\/etc\//\/vendor\/etc\//g' \
-        "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/"$1"
+        "$COMMON_BLOB_ROOT"/"$1"
 }
 
 fix_camera_etc_path vendor/lib/libmmcamera_imglib.so
@@ -79,12 +81,18 @@ fix_camera_etc_path vendor/lib/libopcamera_native_modules.so
 function fix_radio_framework_path () {
     sed -i \
         's/\/system\/framework\//\/vendor\/framework\//g' \
-        "$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary/"$1"
+        "$COMMON_BLOB_ROOT"/"$1"
 }
 
 fix_radio_framework_path vendor/etc/permissions/embms.xml
 fix_radio_framework_path vendor/etc/permissions/qcnvitems.xml
 fix_radio_framework_path vendor/etc/permissions/qcrilhook.xml
 fix_radio_framework_path vendor/etc/permissions/telephonyservice.xml
+
+#
+# Correct android.hidl.manager@1.0-java jar name
+#
+sed -i "s|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g" \
+    "$COMMON_BLOB_ROOT"/vendor/etc/permissions/qti_libpermissions.xml
 
 "$MY_DIR"/setup-makefiles.sh
