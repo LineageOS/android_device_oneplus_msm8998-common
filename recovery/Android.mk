d47f5a0549db16073d601f7b35784d450e380b68
YumeMichi <do4suki@gmail.com>
2018-08-10 04:15:00 +0200

    msm8998: update recovery_updater for P
    Change-Id: I28d1133fcee0ea8010e568fda8ba6bdebb64e338

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -7,8 +7,9 @@ LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
 endif
 
 LOCAL_C_INCLUDES := \
-    $(call project-path-for,recovery) \
-    $(call project-path-for,recovery)/updater/include \
+    $(call project-path-for,recovery)/edify/include \
+    $(call project-path-for,recovery)/otautil/include \
+    $(call project-path-for,recovery)/updater/include
 
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_oneplus
