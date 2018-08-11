b823d2a3f59cfc602fc0d8c80370166ea86965f2
LuK1337 <priv.luk@gmail.com>
2018-08-11 16:27:29 +0200

    msm8998-common: Switch to new NXP NFC
    Change-Id: I680df3c6b79eb47bc827761bb390ee2cfa26f6a1

--- common.mk
+++ common.mk
@@ -312,11 +312,10 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
-    android.hardware.nfc@1.0-service \
+    android.hardware.nfc@1.1-service \
     com.android.nfc_extras \
-    nfc_nci.msm8998 \
     NfcNci \
+    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
