4ffc473d947bb02f913ba92e2225f1856254bab1
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-08-13 12:31:48 +0200

    msm8998: selinux permissive for bringup
    Change-Id: If4cf3242de4c856d1d8574c813eeb2703ba83339

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,7 +67,7 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_ena
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048 androidboot.usbconfigfs=true 
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3 
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
