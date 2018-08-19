6b69cd855d25118a7b39f02aea4c371d2f8114b8
Andre Eisenbach <eisenbach@google.com>
2018-08-18 20:43:27 -0400

    msm8998: Rename libnfc-brcm.conf -> libnfc-nci.conf
    Test: manual
    Merged-In: I213034b54724b1ed0f43b32bc757ea7d8873ee74
    Change-Id: I213034b54724b1ed0f43b32bc757ea7d8873ee74
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- common.mk
+++ common.mk
@@ -320,7 +320,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # OMX
