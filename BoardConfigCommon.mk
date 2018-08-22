cae621395f4405ef32a3c34beda7abd4caebd500
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-08-22 14:51:19 +0200

    msm8998: move ifaa back to system
    this isn't really treble ready
    
    Change-Id: Ibb86b33f98e3288f3563d75aed8d41f2f4986a22

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -275,7 +275,9 @@ BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware persist
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
 
 # Sensors
