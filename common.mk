21524c50cdd3c2808e6b8996c769397f9b767162
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-08-22 13:11:32 +0200

    msm8998: update vendor patch level
    Change-Id: I5743538a330d6e8c63dd046dc352c4ef233e6286

--- common.mk
+++ common.mk
@@ -422,7 +422,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2018-07-01
+    ro.lineage.build.vendor_security_patch=2018-08-01
 
 # Vibrator
 PRODUCT_PACKAGES += \
