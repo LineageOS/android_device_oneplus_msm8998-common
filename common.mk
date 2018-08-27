ae024f945cf2b3ce2ad40c0fbe9385b5cf874c38
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-08-27 18:11:22 +0200

    msm8998: switch to hidl ANT+ hal
    Change-Id: I6ef945fc64f3d8f4c31d89bcdf684333aae99595
    (cherry picked from commit e52599da1a4c3d8e7411521a67fd459362bca27d)

--- common.mk
+++ common.mk
@@ -126,16 +126,10 @@ PRODUCT_COPY_FILES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    com.dsi.ant.antradio_library
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
     libbt-vendor \
     libbthost_if \
     bt-mac-generator
