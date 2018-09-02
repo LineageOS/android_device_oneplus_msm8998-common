e4a19a8f1c20ff9799c2660fd456cb5ec8ffe064
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-02 12:21:32 -0600

    msm8998: Build a system version of android.hidl.base@1.0 for imsvt
    Change-Id: I5aa2e747b6ec4138c9c135c34a1975016e663b79

--- libhidl/Android.mk
+++ libhidl/Android.mk
@@ -14,34 +14,26 @@
 # limitations under the License.
 
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-
-LOCAL_SHARED_LIBRARIES := \
-    libhidltransport
-
-LOCAL_C_INCLUDES := \
-
+LOCAL_SHARED_LIBRARIES := libhidltransport
 LOCAL_MODULE := android.hidl.base@1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_VENDOR_MODULE := true
-
 include $(BUILD_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := libhidltransport
+LOCAL_MODULE := android.hidl.base@1.0_system
+LOCAL_INSTALLED_MODULE_STEM := android.hidl.base@1.0.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_SHARED_LIBRARY)
 
-LOCAL_SRC_FILES := \
-
-LOCAL_SHARED_LIBRARIES := \
-    libhidltransport
-
-LOCAL_C_INCLUDES := \
-
+include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := libhidltransport
 LOCAL_MODULE := android.hidl.manager@1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_VENDOR_MODULE := true
-
 include $(BUILD_SHARED_LIBRARY)
