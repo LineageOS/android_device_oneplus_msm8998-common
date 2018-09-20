3b95aa9bad349cf51e97feab029e0b6992b9efe9
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-09-20 19:07:33 +0200

    msm8998: move bluetooth mac to /data/vendor/
    Change-Id: I550c1e4f5a088758bc965b88935fc1fae48ebded

--- bt-mac-generator/bt_mac_generator.cpp
+++ bt-mac-generator/bt_mac_generator.cpp
@@ -30,7 +30,7 @@
 
 static int force_random = 0;
 static const char NV_MAC_FILE[]          = "/data/oemnvitems/447";
-static const char BDADDR_PATH[]          = "/data/misc/bluetooth/bdaddr";
+static const char BDADDR_PATH[]          = "/data/vendor/bluetooth/bdaddr";
 
 static void to_upper(char *str) {
     int i = 0;
