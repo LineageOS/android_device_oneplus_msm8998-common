dc98d2e0aa2004f80f4c89e6f8febd7cd74e3944
Anton Hansson <hansson@google.com>
2018-11-07 16:09:35 +0000

    msm8998-common: Remove obsolete dex settings
    This block is completely redundant because..
    - WITH_DEXPREOPT := true is the default
    - WITH_DEXPREOPT_PIC is never read
    - DEX_PREOPT_DEFAULT := nostripping has no effect when the
      BOARD_USES_SYSTEM_OTHER_ODEX flag is set to true
    
    Test: make
    Change-Id: I618a1e395ca1a14555cd50eef1a4f1aa079b553d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -124,16 +124,6 @@ OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
-
 # Filesystem
 TARGET_FS_CONFIG_GEN += $(PLATFORM_PATH)/config.fs
 
