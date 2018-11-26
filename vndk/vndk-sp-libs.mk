da72554239a940c532340db8d987dcb879c2f26b
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-11-26 20:35:43 +0100

    [DNM] msm8998: update blobs from 9.0 beta
    Change-Id: I0d2e34bae5728dd3af36a95054546560b970606e

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -1,10 +1,14 @@
 VNDK_SP_LIBRARIES := \
     android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.mapper@2.0 \
+    android.hardware.graphics.mapper@2.1 \
     android.hardware.graphics.common@1.0 \
+    android.hardware.graphics.common@1.1 \
     android.hardware.renderscript@1.0 \
     android.hidl.base@1.0 \
     android.hidl.memory@1.0 \
+    android.hidl.memory@1.0-impl \
+    android.hidl.memory.token@1.0 \
     libRSCpuRef \
     libRSDriver \
     libRS_internal \
@@ -28,4 +32,5 @@ VNDK_SP_LIBRARIES := \
     libunwindstack \
     libutils \
     libutilscallstack \
-    libdexfile
+    libdexfile \
+    libz
