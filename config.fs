3b5a24e27d4cd03dcac710828e89a400bad55b0b
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-11-26 20:35:43 +0100

    msm8998: update blobs from 9.0 beta
    Change-Id: I0d2e34bae5728dd3af36a95054546560b970606e

--- config.fs
+++ config.fs
@@ -1,14 +1,20 @@
 [AID_VENDOR_QTI_DIAG]
 value:2901
 
-[AID_VENDOR_QCOM_DIAG]
-value:2950
+[AID_VENDOR_QDSS]
+value:2902
 
 [AID_VENDOR_RFS]
-value:2951
+value:2903
 
 [AID_VENDOR_RFS_SHARED]
-value:2952
+value:2904
+
+[AID_VENDOR_ADPL_ODL]
+value:2905
+
+[AID_VENDOR_QRTR]
+value:2906
 
 [vendor/bin/wcnss_filter]
 mode: 0755
@@ -50,7 +56,7 @@ caps: NET_BIND_SERVICE
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
-caps: WAKE_ALARM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [vendor/bin/cnd]
 mode: 0755
@@ -64,9 +70,39 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
 
+[vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
+[vendor/bin/xtwifi-client]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[vendor/bin/sensors.qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
 [firmware/]
 mode: 0771
 user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[vendor/firmware_mnt/image/*]
+mode: 0771
+user: AID_ROOT
 group: AID_SYSTEM
 caps: 0
 
