93fd01e2a3e3de31c598160db36272fe9d29f4ed
Pranav Vashi <neobuddy89@gmail.com>
2018-12-11 01:18:37 +0530

    msm8998: Fix symlink for ims libs
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ie9f3851f78bb7bb6a10d617607363fe16879868e

--- Android.mk
+++ Android.mk
@@ -34,7 +34,7 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(PRODUCT_OUT)/system/priv-app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
