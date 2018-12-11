a9c27e11db58b26430a78c25d7ac08e92dc828de
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-12-11 19:34:57 +0100

    msm8998: wifi: start wifi driver on boot
    Change-Id: I99337452a266e57e074f42cbb3db15c3c7ad11b7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -317,6 +317,9 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
+WIFI_DRIVER_STATE_CTRL_PARAM := "/sys/kernel/boot_wlan/boot_wlan"
+WIFI_DRIVER_STATE_OFF := 0
+WIFI_DRIVER_STATE_ON := 1
 
 # inherit from the proprietary version
 -include vendor/oneplus/msm8998-common/BoardConfigVendor.mk
