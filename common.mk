bc83c58f6f37d17f777f0e7d71d5b8a088cb601a
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    msm8998-common: Adding Audio HAL V5: Direction API
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>
    Change-Id: I9aa45829e7e1149c1406d20b1e08ce5dd135fafd

--- common.mk
+++ common.mk
@@ -89,9 +89,9 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     audio.a2dp.default \
     audio_amplifier.msm8998 \
     audio.primary.msm8998 \
