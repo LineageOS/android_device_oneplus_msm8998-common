1364f5164f0b8f23cd851b5117ae218103b59467
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-12-24 14:38:18 +0100

    msm8998: build clearkey from sources
    Change-Id: I6df0b3803b604c01a56c265439d479426042ccdc

--- common.mk
+++ common.mk
@@ -187,7 +187,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.0-service.widevine
+    android.hardware.drm@1.1-service.clearkey \
+    android.hardware.drm@1.0-service.widevine \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
