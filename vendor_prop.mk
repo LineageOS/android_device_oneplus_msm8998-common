62dd6937cd0a75280796801db5a9bddd10ae44cb
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-01-02 13:19:51 +0100

    msm8998: disable ubwc for wfd
    Change-Id: I7d16563d6a2a6341b4bc8db4cd7071a22c54a8f9

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -129,7 +129,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gralloc.enable_fb_ubwc=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=0 \
-    vendor.display.perf_hint_window=50
+    vendor.display.perf_hint_window=50 \
+    vendor.video.disable.ubwc=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
