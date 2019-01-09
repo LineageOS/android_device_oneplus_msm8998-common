b9a8ad80d200119685f986bd14b1a38a817fcd6e
shoudil <stephenli@codeaurora.org>
2019-01-09 17:04:11 +0800

    msm8998-common: Add framework detect modules to PRODUCT_PACKAGES
    Add JNI and libqti_vndfwk_detect module name to product_packages
    to specify module to be installed to device. Here explicitly
    specify both the core and vendor variant in PRODUCT_PACKAGES.
    
    CRs-Fixed: 2372775
    
    Change-Id: Ic64f5ac0cfbbe9e105a6b48b5bc7687c8d23d7e8

--- common.mk
+++ common.mk
@@ -218,6 +218,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Framework detect
+PRODUCT_PACKAGES += \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
+
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
