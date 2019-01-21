a46bce132d9050e18c71d94455cbbc388f1c37f5
Bruno Martins <bgcngm@gmail.com>
2019-01-21 09:40:51 +0000

    [WIP] msm8998-common: Migrate to livedisplay 2.0
    Based on the sdm845 implementation from Danny Baumann.
    https://github.com/LineageOS/android_device_oneplus_sdm845-common/commit/4203f3ec180ea65c74ed79d7d9dc87485537998c
    
    Change-Id: Ib39e4646e5fb5aca73d4ebc54e841f859ae0c0d1

---
+++ livedisplay/service.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "vendor.lineage.livedisplay@2.0-service.oneplus_msm8998"
+
+#include <android-base/logging.h>
+#include <binder/ProcessState.h>
+#include <hidl/HidlTransportSupport.h>
+#include "DisplayModes.h"
+
+using ::vendor::lineage::livedisplay::V2_0::IDisplayModes;
+using ::vendor::lineage::livedisplay::V2_0::implementation::DisplayModes;
+
+int main() {
+    android::sp<IDisplayModes> modesService = new DisplayModes();
+
+    android::hardware::configureRpcThreadpool(2, true /*callerWillJoin*/);
+
+    if (modesService->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register display modes HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "LiveDisplay HAL service ready.";
+
+    android::hardware::joinRpcThreadpool();
+
+    LOG(ERROR) << "LiveDisplay HAL service failed to join thread pool.";
+    return 1;
+}
