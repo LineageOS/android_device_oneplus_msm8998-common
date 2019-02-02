725b77221b9811df8ae3bcecc28f8bd55f569352
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-02-02 12:38:21 +0100

    msm8998-common: Replace lineagehw implementation by HIDL services
    Move DisplayModeControl into a livedisplay
    service, and TouchscreenGestures into a touch one.
    
    Based on the sdm845 implementation from Danny Baumann.
    https://github.com/LineageOS/android_device_oneplus_sdm845-common/commit/4203f3ec180ea65c74ed79d7d9dc87485537998c
    
    Change-Id: Ib39e4646e5fb5aca73d4ebc54e841f859ae0c0d1

--- light/service.cpp
+++ light/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.light@2.0-service.oneplus_8998"
+#define LOG_TAG "android.hardware.light@2.0-service.oneplus_msm8998"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
