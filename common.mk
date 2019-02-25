c95d30a0792892e656c8691bb93829804255f48b
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-02-25 17:58:19 +0100

    msm8998-common: build versioned vndk
    Change-Id: I0a3d40619819e45f8193d20a632fe36714f3d057

--- common.mk
+++ common.mk
@@ -173,7 +173,8 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml \
     libqdMetaData \
-    libqdMetaData.system
+    libqdMetaData.system \
+    libvulkan
 
 PRODUCT_PACKAGES += \
     vendor.display.config@1.0 \
@@ -451,12 +452,11 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service
 
 # VNDK
+# Update this list with what each blob is actually for
+# libstdc++: camera.msm8998
 PRODUCT_PACKAGES += \
+    libstdc++.vendor \
     vndk_package
-
-# VNDK-SP
-PRODUCT_PACKAGES += \
-    vndk-sp
 
 # VR
 PRODUCT_PACKAGES += \
