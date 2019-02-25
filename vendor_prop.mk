2749bf68df5dbd24176b94ef28d5f1e2ddfc3ba0
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-02-25 18:57:25 +0100

    msm8998-common: update props from 9.0.3
    Change-Id: Ia3bdb0bf972082a4fd7a43da45f80d9c35cbc723

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -94,7 +94,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Connor
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.connor.disable=0
+    persist.vendor.connor.disable=1
 
 # Core control
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -103,9 +103,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Data
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.use_data_netmgrd=true \
     persist.data.netmgrd.qos.enable=true \
-    persist.data.mode=concurrent
+    persist.vendor.data.mode=concurrent \
+    ro.vendor.use_data_netmgrd=true
 
 # DPM
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -207,13 +207,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=true \
     persist.sys.oem_smooth=1 \
     persist.radio.VT_CAM_INTERFACE=1 \
-    persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.efssync=true \
     persist.radio.multisim.config=dsds \
     persist.radio.enhance_ecall=true \
     persist.radio.hw_mbn_update=0 \
     persist.radio.sw_mbn_update=0 \
     persist.radio.start_ota_daemon=0 \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.data_con_rprt=1 \
     persist.vendor.radio.data_ltd_sys_ind=1 \
