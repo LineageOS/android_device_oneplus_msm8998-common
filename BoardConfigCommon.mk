370f0b81e2c934252653a55eb722b78795d116f8
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-04-22 07:42:31 +0200

    msm8998-common: enable Wi-Fi STA/AP concurrency
    https: //source.android.com/devices/tech/connect/wifi-sta-ap-concurrency
    Change-Id: Ife43c49b45f9158da8fddfa1764864ee0cbafd9f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -313,11 +313,12 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
-WPA_SUPPLICANT_VERSION := VER_0_8_X
 WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
 WIFI_DRIVER_STATE_CTRL_PARAM := "/sys/kernel/boot_wlan/boot_wlan"
 WIFI_DRIVER_STATE_OFF := 0
 WIFI_DRIVER_STATE_ON := 1
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
 -include vendor/oneplus/msm8998-common/BoardConfigVendor.mk
