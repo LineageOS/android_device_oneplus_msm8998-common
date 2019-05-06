787db6505be2776e68f92bceb2a240eff349f986
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-05-06 20:01:51 +0200

    msm8998-common: build libstdc++.vendor for camera hal
    Change-Id: I25c11cb5545fb38902eab322d860b3374be6f9ce

--- common.mk
+++ common.mk
@@ -141,6 +141,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
+    libstdc++.vendor \
     Snap
 
 PRODUCT_PACKAGES += \
