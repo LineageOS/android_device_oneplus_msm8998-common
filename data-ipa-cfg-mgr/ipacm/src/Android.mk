8aa86b183a1ab1ac8cbb1287c9925253c112bace
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-06-16 08:39:07 +0200

    msm8998-common: update data-ipa-cfg-mgr from LA.UM.7.4.r1-05300-8x98.0
    Change-Id: Ic770245bdba6d051daa90fa350ae5e4069718781

--- data-ipa-cfg-mgr/ipacm/src/Android.mk
+++ data-ipa-cfg-mgr/ipacm/src/Android.mk
@@ -6,6 +6,7 @@ BOARD_IPAv3_LIST += sdm845
 BOARD_IPAv3_LIST += sdm710
 BOARD_IPAv3_LIST += msmnile
 BOARD_IPAv3_LIST += $(MSMSTEPPE)
+BOARD_IPAv3_LIST += $(TRINKET)
 
 ifneq ($(call is-board-platform-in-list,$(BOARD_PLATFORM_LIST)),true)
 ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
@@ -24,6 +25,8 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_CFLAGS := -v
 LOCAL_CFLAGS += -DFEATURE_IPA_ANDROID
 LOCAL_CFLAGS += -DFEATURE_IPACM_RESTART
+LOCAL_CFLAGS += -DFEATURE_ETH_BRIDGE_LE
+
 LOCAL_CFLAGS += -DFEATURE_IPACM_HAL -Wall -Werror -Wno-error=macro-redefined
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 LOCAL_CFLAGS += -DDEBUG
@@ -57,7 +60,8 @@ LOCAL_SRC_FILES := IPACM_Main.cpp \
 		IPACM_ConntrackClient.cpp \
 		IPACM_ConntrackListener.cpp \
 		IPACM_Log.cpp \
-		IPACM_OffloadManager.cpp
+		IPACM_OffloadManager.cpp \
+		IPACM_LanToLan.cpp
 
 LOCAL_MODULE := ipacm
 LOCAL_CLANG := false
