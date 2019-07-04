7ec28459e5b9879bde3aeec5a11460d4516aec17
LuK1337 <priv.luk@gmail.com>
2019-07-04 11:51:05 +0100

    msm8998-common: pocketmode: Kill off Lineage SDK dependency
    Change-Id: I05f35124be4547b7fbc3ff3aa145bf27600454e4

--- pocketmode/src/org/lineageos/pocketmode/PocketSensor.java
+++ pocketmode/src/org/lineageos/pocketmode/PocketSensor.java
@@ -22,11 +22,12 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.FileUtils;
 import android.os.SystemProperties;
 import android.text.TextUtils;
 import android.util.Log;
 
-import org.lineageos.internal.util.FileUtils;
+import java.io.IOException;
 
 public class PocketSensor implements SensorEventListener {
 
@@ -75,10 +76,10 @@ public class PocketSensor implements SensorEventListener {
     }
 
     private void setFPProximityState(boolean isNear) {
-        if (FileUtils.isFileWritable(FPC_FILE)) {
-            FileUtils.writeLine(FPC_FILE, isNear ? "1" : "0");
-        } else {
-            Log.e(TAG, "Proximity state file " + FPC_FILE + " is not writable!");
+        try {
+            FileUtils.stringToFile(FPC_FILE, isNear ? "1" : "0");
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to write to " + FPC_FILE, e);
         }
     }
 
