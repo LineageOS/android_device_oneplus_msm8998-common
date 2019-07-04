6001f799e9e89006f2da83b491927042806eaf49
Giuseppe Barillari <joe2k01dev@gmail.com>
2019-07-04 10:35:24 +0200

    msm8998-common: init: Remove unused function
    Change-Id: Iee68edb5282024ad1e67251e57ddb635da959f1d

--- init/init_oneplus_msm8998.cpp
+++ init/init_oneplus_msm8998.cpp
@@ -55,23 +55,6 @@ using android::base::GetProperty;
 using android::base::ReadFileToString;
 using android::init::property_set;
 
-void property_override(const std::string& name, const std::string& value)
-{
-    size_t valuelen = value.size();
-
-    prop_info* pi = (prop_info*) __system_property_find(name.c_str());
-    if (pi != nullptr) {
-        __system_property_update(pi, value.c_str(), valuelen);
-    }
-    else {
-        int rc = __system_property_add(name.c_str(), name.size(), value.c_str(), valuelen);
-        if (rc < 0) {
-            LOG(ERROR) << "property_set(\"" << name << "\", \"" << value << "\") failed: "
-                       << "__system_property_add failed";
-        }
-    }
-}
-
 void init_target_properties()
 {
     std::string device;
