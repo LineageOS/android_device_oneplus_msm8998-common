07ad3e2ccbb06564ea95e630ed3ac60ecb07bfbb
LuK1337 <priv.luk@gmail.com>
2019-07-08 15:47:03 +0200

    msm8998-common: Replace wifi mac generator with a shell script
    Change-Id: I95593db840dc4a668f899d248b2c8e03d4ebbc3e

---
+++ wifi-mac-generator/wifi-mac-generator.sh
@@ -0,0 +1,43 @@
+#!/vendor/bin/sh
+WLAN_MAC_VENDOR_PREFIX="C0EEFB"
+WLAN_MAC_PERSIST_PATH="/mnt/vendor/persist/wlan_mac.bin"
+
+function wait_for_file() {
+    retries=0
+
+    while [ ! -f "${1}" ]; do
+        retries=$((retries + 1))
+
+        if [ "${retries}" -eq 10 ]; then
+            return 1
+        fi
+
+        sleep 1
+    done
+
+    return 0
+}
+
+if [[ ! -f "${WLAN_MAC_PERSIST_PATH}" ]] || [[ ! -s "${WLAN_MAC_PERSIST_PATH}" ]]; then
+    MAC_0_PATH="/data/vendor/oemnvitems/4678_0"
+
+    if ! wait_for_file "${MAC_0_PATH}"; then
+        MAC_0="${WLAN_MAC_VENDOR_PREFIX}`xxd -l 3 -p /dev/urandom | tr '[:lower:]' '[:upper:]'`"
+    else
+        MAC_0=`xxd -p "${MAC_0_PATH}" | tr '[:lower:]' '[:upper:]'`
+    fi
+
+    MAC_1_PATH="/data/vendor/oemnvitems/4678_1"
+
+    if ! wait_for_file "${MAC_1_PATH}"; then
+        MAC_1="${WLAN_MAC_VENDOR_PREFIX}`xxd -l 3 -p /dev/urandom | tr '[:lower:]' '[:upper:]'`"
+    else
+        MAC_1=`xxd -p "${MAC_1_PATH}" | tr '[:lower:]' '[:upper:]'`
+    fi
+
+    echo "Intf0MacAddress=${MAC_0}" > "${WLAN_MAC_PERSIST_PATH}"
+    echo "Intf1MacAddress=${MAC_1}" >> "${WLAN_MAC_PERSIST_PATH}"
+    echo "Intf2MacAddress=000AF58989FD" >> "${WLAN_MAC_PERSIST_PATH}"
+    echo "Intf3MacAddress=000AF58989FC" >> "${WLAN_MAC_PERSIST_PATH}"
+    echo "END" >> "${WLAN_MAC_PERSIST_PATH}"
+fi
