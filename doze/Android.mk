d244e2f9a585754fc542f6933d4e9fda54150d77
Bruno Martins <bgcngm@gmail.com>
2019-07-09 22:31:00 +0100

    msm8998-common: Doze: Migrate to androidx
    Change-Id: Ifaa67283a3437549a2304088a0ce377b399da19f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- doze/Android.mk
+++ doze/Android.mk
@@ -13,12 +13,7 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v4 \
-    android-support-v13 \
-    android-support-v7-recyclerview \
-    android-support-v7-preference \
-    android-support-v7-appcompat \
-    android-support-v14-preference
+    androidx.preference_preference
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
