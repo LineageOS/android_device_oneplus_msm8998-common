5d3b23b9423a2cd442172ca03adee49fc684905f
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:20:32 +0430

    msm8998-common: Drop module eng tags
    Change-Id: I890456cb7b3b8932b41501d1a84353c0fb903925

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -14,7 +14,7 @@ LOCAL_C_INCLUDES := \
 
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_oneplus
-LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_TAGS  := optional
 
 include $(BUILD_STATIC_LIBRARY)
 
