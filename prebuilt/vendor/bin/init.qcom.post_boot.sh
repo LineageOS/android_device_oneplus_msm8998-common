965ec81feb927b371bfed0a7435f4d2e92a00e6b
Pranav Vashi <neobuddy89@gmail.com>
2019-09-16 23:13:07 +0530

    msm8998-common: Update fstab and disable zram
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ib4fced81e1ed81ab5ca069d451fc39960e120cbe

--- prebuilt/vendor/bin/init.qcom.post_boot.sh
+++ prebuilt/vendor/bin/init.qcom.post_boot.sh
@@ -330,7 +330,7 @@ low_ram=`getprop ro.config.low_ram`
 
 if [ "$ProductName" == "msmnile" ]; then
       # Enable ZRAM
-      configure_zram_parameters
+      #configure_zram_parameters
       configure_read_ahead_kb_values
 else
     arch_type=`uname -m`
@@ -423,7 +423,7 @@ else
     echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
     echo 100 > /proc/sys/vm/swappiness
 
-    configure_zram_parameters
+    #configure_zram_parameters
 
     configure_read_ahead_kb_values
 
