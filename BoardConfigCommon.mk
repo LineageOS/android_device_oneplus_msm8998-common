2df27a574246f596c932db132309cfbc68fafd4b
Pranav Vashi <neobuddy89@gmail.com>
2019-09-19 00:37:49 +0530

    msm8998-common: Remove kernel CC prefix
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iaf8aaadab25c8c3f2b1672ac6010048b057a5c09

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,7 +79,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
