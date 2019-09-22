f0be6bc5d450b17edf3fcb21c162c8b8c8639279
Pranav Vashi <neobuddy89@gmail.com>
2019-09-22 17:09:01 +0530

    msm8998-common: build 32-bit android.frameworks.displayservice@1.0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I577b29ad7c006fe1408c4ced39b7de55388946db

--- common.mk
+++ common.mk
@@ -139,6 +139,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
