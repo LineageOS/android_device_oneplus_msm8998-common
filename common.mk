4c25ae6b820f1f59b1b7abbfd9bdf25fd448c9c9
neobuddy89 <neobuddy89@gmail.com>
2019-09-26 00:03:59 +0530

    DNM: msm8998-common: Disable audio amplifier
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>
    Change-Id: If995baecf80c1cfa52a5b603166ce92afc4b9224

--- common.mk
+++ common.mk
@@ -93,7 +93,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     audio.a2dp.default \
-    audio_amplifier.msm8998 \
     audio.primary.msm8998 \
     audio.r_submix.default \
     audio.usb.default \
