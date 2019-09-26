29734f854fb3dc4972d035dfe4c2dd1c883acd38
neobuddy89 <neobuddy89@gmail.com>
2019-09-26 21:42:21 +0530

    msm8998-common: Switch to device/qcom/sepolicy-legacy-um policy
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>
    Change-Id: Ieac55bdde97ea6e2c2e6632a40ef975502056204

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -283,7 +283,7 @@ BOARD_ROOT_EXTRA_SYMLINKS += /vendor/firmware_mnt:/firmware
 BOARD_ROOT_EXTRA_SYMLINKS += /mnt/vendor/persist:/persist
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
