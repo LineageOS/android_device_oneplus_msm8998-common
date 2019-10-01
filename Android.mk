f6cb88d76188b22b4cdc8ad1274ce5ce73d39346
Pranav Vashi <neobuddy89@gmail.com>
2019-10-01 22:58:09 +0530

    DNM: msm8998-common: Update IMS stack
    * Remove CNEServiceApp
    * Start moving to /product
    * Add new packages
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ic436e58913cfd538d844b1e1dcf24cd4f6f5633c

--- Android.mk
+++ Android.mk
@@ -34,12 +34,12 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-IMS_SYMLINKS := $(addprefix $(PRODUCT_OUT)/system/priv-app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT)/priv-app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
+	$(hide) ln -sf /product/lib64/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
