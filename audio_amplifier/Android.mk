6c150a94a12842e3a9f665ca94b9423b6d8f7096
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-10-07 19:32:13 +0200

    msm8998-common: audio_amplifier: add missing include
    Change-Id: Iff7d7c683f3d958dc01b599b49a216cb401e1a48

--- audio_amplifier/Android.mk
+++ audio_amplifier/Android.mk
@@ -22,6 +22,7 @@ LOCAL_C_INCLUDES := \
     external/tinycompress/include \
     hardware/libhardware/include \
     system/media/audio/include \
+    system/media/audio_utils/include \
     $(call include-path-for, audio-route) \
     $(call include-path-for, audio-effects) \
     $(call project-path-for,qcom-audio)/hal \
