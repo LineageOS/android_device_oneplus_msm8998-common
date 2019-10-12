36a44f8ec65e373f84e87c818a5d8bc857da1d4e
Pranav Vashi <neobuddy89@gmail.com>
2019-10-13 00:16:29 +0530

    msm8998-common: Add missing nfc configs from 9.0.8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I238f52e8143e034a2b02434c937d9b1f21d639c6

--- common.mk
+++ common.mk
@@ -321,10 +321,7 @@ PRODUCT_PACKAGES += \
     vendor.nxp.hardware.nfc@1.1-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libese-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libese-nxp.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+    $(call find-copy-subdir-files,*,${LOCAL_PATH}/configs/nfc,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # NFC - Secure Element
 PRODUCT_PACKAGES += \
