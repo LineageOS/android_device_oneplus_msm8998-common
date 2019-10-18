235b26e9e711f099060b22a8819f7a52f5ea188e
dianlujitao <dianlujitao@lineageos.org>
2019-10-18 11:05:40 +0800

    msm8998-common: Enable QC framework value-adds
    Change-Id: Ib85e9a4f68935243918ec876ba9be16230cf30fd

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -288,6 +288,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.qcomsysd.enabled=1
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.va_aosp.support=1
+
+PRODUCT_ODM_PROPERTIES += \
+    ro.vendor.qti.va_odm.support=1
+
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
     DEVICE_PROVISIONED=1 \
