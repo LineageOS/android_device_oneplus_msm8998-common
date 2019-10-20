4943e7795cfcf440cf77f45f647676a50c199c33
Ethan Chen <intervigil@gmail.com>
2019-10-20 01:17:32 -0700

    msm8998-common: Remove dead RCS packages
    Change-Id: Ie2c03fa11cb08c7ab900d7b666981d592aba19c7

--- common.mk
+++ common.mk
@@ -366,13 +366,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
-# RCS
-PRODUCT_PACKAGES += \
-    rcs_service_aidl \
-    rcs_service_aidl.xml \
-    rcs_service_api \
-    rcs_service_api.xml
-
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_oneplus
