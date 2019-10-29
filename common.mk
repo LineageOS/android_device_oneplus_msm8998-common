0a79b3433fa6e3eff32c0732c03f837fd3da1b51
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-10-29 17:30:19 +0100

    msm8998-common: update blobs from 9.0.9
    Change-Id: I1aabeb544fb7e307ea8215b10c2c3eab7328d874

--- common.mk
+++ common.mk
@@ -439,7 +439,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2019-08-01
+    ro.lineage.build.vendor_security_patch=2019-10-01
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
