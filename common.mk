759a6ab4c3024079f5756287366e7540da5db49f
Simao Gomes Viana <xdevs23@outlook.com>
2019-12-21 12:15:22 +0100

    use fw/native 8192 dalvik-heap
    Depends-On: Icdfd3512a79d9935d3fdb611c96dc8fd91a5e1c5
    Change-Id: I6500ccfefea2a6bf794f623108490b7437ee8d40
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- common.mk
+++ common.mk
@@ -29,13 +29,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapmaxfree=8m
+$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
