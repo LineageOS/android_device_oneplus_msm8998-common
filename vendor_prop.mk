e097f6263618f2d108c0a7bff9816e33577f877e
Ethan Chen <intervigil@gmail.com>
2020-03-22 14:11:11 -0700

    msm8998-common: Remove unnecessary init.qcom.usb.sh configuration
    * Move what is actually executed into vendor_prop.mk and delete the rest
      of the script as it is actually unused or irrelevant.
    
    Change-Id: I76a4af1f054b64418ee3b63354363a4e38f82041

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -302,7 +302,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.usb.controller=a800000.dwc3 \
     sys.usb.rndis.func.name=gsi \
-    sys.usb.rmnet.func.name=gsi
+    sys.usb.rmnet.func.name=gsi \
+    persist.vendor.usb.config.extra=none \
+    vendor.usb.rps_mask=0
 
 # VNDK
 PRODUCT_PROPERTY_OVERRIDES += \
