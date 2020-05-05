fb919043bd1383dfc22f1b7222f0be7b4d477c42
Pranav Vashi <neobuddy89@gmail.com>
2020-05-06 03:45:37 +0530

    msm8998-common: Add missing 64-bit interfaces required for IMS
    Change-Id: I8ef9171cd5d53cfb5ddfaae49b56696940ae6442

--- common.mk
+++ common.mk
@@ -146,6 +146,10 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
+    android.hardware.camera.device@3.4:64 \
+    android.hardware.camera.device@3.5:64 \
+    android.hardware.camera.provider@2.4:64 \
+    android.hardware.camera.provider@2.5:64 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
