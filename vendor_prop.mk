e0d783730622a49eb55adf725f5a2f1865a0c544
Ethan Chen <intervigil@gmail.com>
2020-05-09 20:36:49 -0700

    msm8998-common: Move static properties to system properties file
    Change-Id: If8543cf1d658c4e938eeec78f6a2d3685e756ad4

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -246,6 +246,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.iwlan_operation_mode=legacy \
     keyguard.no_require_sim=true \
     ro.com.android.dataroaming=true \
+    ro.telephony.call_ring.multiple=false \
     persist.sys.oem_smooth=1 \
     persist.radio.multisim.config=dsds \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
