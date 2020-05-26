97ecc36a84a6c535c478aee59a4102c73ce9cccd
Pranav Vashi <neobuddy89@gmail.com>
2020-05-26 18:29:18 +0530

    msm8998-common: Copy native public libraries
    Change-Id: I3c50aea24710debd3ec411eb8fa4851d5f30b22d

--- common.mk
+++ common.mk
@@ -309,6 +309,10 @@ PRODUCT_PACKAGES += \
     libregistermsext \
     mediametrics
 
+# Native Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Netutils
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
