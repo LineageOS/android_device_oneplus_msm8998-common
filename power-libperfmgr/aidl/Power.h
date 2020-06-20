bcf4533c1cad33f76976a7a93b8808bdaa5bed32
Midas Chien <midaschieh@google.com>
2020-06-20 17:52:03 +0800

    msm8998-common: power: display: cache display lpm state to avoid set it again
    If current foss state same as desired state, doesn't set it again.
    
    Bug: 148656663
    Test: stress on/off battery saver and check foss state
    Test: enable battery saver, stress on/off screen
    Change-Id: I6298361fd6a5d6a86c94eabb71ab254507136fd3

--- power-libperfmgr/aidl/Power.h
+++ power-libperfmgr/aidl/Power.h
@@ -23,6 +23,7 @@
 #include <aidl/android/hardware/power/BnPower.h>
 #include <perfmgr/HintManager.h>
 
+#include "disp-power/DisplayLowPower.h"
 #include "disp-power/InteractionHandler.h"
 
 namespace aidl {
@@ -39,7 +40,7 @@ using ::android::perfmgr::HintManager;
 
 class Power : public ::aidl::android::hardware::power::BnPower {
   public:
-    Power(std::shared_ptr<HintManager> hm);
+    Power(std::shared_ptr<HintManager> hm, std::shared_ptr<DisplayLowPower> dlpw);
     ndk::ScopedAStatus setMode(Mode type, bool enabled) override;
     ndk::ScopedAStatus isModeSupported(Mode type, bool *_aidl_return) override;
     ndk::ScopedAStatus setBoost(Boost type, int32_t durationMs) override;
@@ -48,6 +49,7 @@ class Power : public ::aidl::android::hardware::power::BnPower {
 
   private:
     std::shared_ptr<HintManager> mHintManager;
+    std::shared_ptr<DisplayLowPower> mDisplayLowPower;
     std::unique_ptr<InteractionHandler> mInteractionHandler;
     std::atomic<bool> mVRModeOn;
     std::atomic<bool> mSustainedPerfModeOn;
