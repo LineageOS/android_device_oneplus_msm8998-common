9d4fa974161df3a049954f0556c5f3e05b042763
dianlujitao <dianlujitao@lineageos.org>
2020-09-13 16:15:39 +0800

    msm8998-common: Provide shim for libdpmframework.so
    Change-Id: Ic5d732f3e98ba45c886db9646d6ef30eb0d42bf4

--- extract-files.sh
+++ extract-files.sh
@@ -52,6 +52,14 @@ if [ -z "$SRC" ]; then
     SRC=adb
 fi
 
+function blob_fixup() {
+    case "${1}" in
+    product/lib64/libdpmframework.so | product/lib/libdpmframework.so )
+        patchelf --add-needed "libshim_dpmframework.so" "${2}"
+        ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true "$CLEAN_VENDOR"
 
