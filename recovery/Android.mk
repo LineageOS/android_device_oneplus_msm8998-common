790a157ec74c23b481b8deb5824480393a556ede
LuK1337 <priv.luk@gmail.com>
2020-09-21 16:24:33 +0200

    msm8998-common: Fix recovery extension build errors
    Change-Id: I0ae36fc7738dcfca64ddee3cf1d7dc8f34818267

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -10,7 +10,8 @@ LOCAL_C_INCLUDES := \
     bootable/recovery \
     bootable/recovery/edify/include \
     bootable/recovery/otautil/include \
-    bootable/recovery/updater/include
+    bootable/recovery/updater/include \
+    system/core/base/include
 
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_oneplus
