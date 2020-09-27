3fc41d22328f4dcbd053d0d88470e0021a0783db
LuK1337 <priv.luk@gmail.com>
2020-09-27 17:45:33 +0200

    msm8998-common: Use libcutils-v29.so for libdpmframework.so
    * v30 one doesn't have `strdup8to16` symbol in it.
    
    Change-Id: Id2b4bb3017bc24c817dbe4b03834960aa1a4b444

--- common.mk
+++ common.mk
@@ -498,6 +498,10 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor \
     vndk_package
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcutils-v29.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
+
 # VR
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \
