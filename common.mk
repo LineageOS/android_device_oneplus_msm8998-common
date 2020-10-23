9e0800503feac6d4ec170552f1de509ae5ab6758
Timi <timi.rautamaki@gmail.com>
2020-10-23 13:21:58 +0200

    msm8998-common: overlay: clean up, enforce RRO
     * Seperate Lineage-specific overlays to overlay-lineage
     * Remove inexistent overlays
     * Remove default touchscreen gestures
     * Remove overlays that set the default value
    
     * Exclude lineage-sdk overlays from RRO
    
    Change-Id: Ic88537d5d811e37e7d298b0c8766af8a67f41c87

--- common.mk
+++ common.mk
@@ -29,7 +29,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_TARGETS += *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
+    device/oppo/common/overlay/lineage-sdk
 
 # Permissions
 PRODUCT_COPY_FILES += \
