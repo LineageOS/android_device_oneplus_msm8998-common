cf4eba22a287dd87ce9385c742601e694205f439
Timi <timi.rautamaki@gmail.com>
2020-10-23 13:21:58 +0200

    msm8998-common: clean up, enforce RRO for overlays
     * Split Lineage-specific overlays to overlay-lineage
     * Remove inexistent overlays
     * Remove default touchscreen gestures
     * Remove overlays that set the default value
    
     * Exclude lineage-sdk overlays from RRO
    
    Change-Id: Ie5a079f5b2dca88dd107afd7b4f1ca9ccb225a1d

--- common.mk
+++ common.mk
@@ -29,7 +29,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_TARGETS += *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    device/oneplus/cheeseburger/overlay/lineage-sdk \
+    device/oneplus/dumpling/overlay/lineage-sdk \
+    device/oppo/common/overlay/lineage-sdk
 
 # Permissions
 PRODUCT_COPY_FILES += \
