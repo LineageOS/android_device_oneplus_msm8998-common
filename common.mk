5b320b4109cd1ff9e43fe5fc019b0d674bdfed65
Timi <timi.rautamaki@gmail.com>
2020-10-29 20:15:53 +0100

    msm8998-common: don't exclude lineage-sdk from RRO
     * Change https://github.com/LineageOS/android_frameworks_base/commit/6c8a5ea387c81e0086b2fd55944334f218fd0e37
    
    Change-Id: I1c7713127ce350b88660921ec06f59fd36ac3142

--- common.mk
+++ common.mk
@@ -35,9 +35,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
     $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
-    device/oppo/common/overlay/lineage-sdk
 
 # Permissions
 PRODUCT_COPY_FILES += \
