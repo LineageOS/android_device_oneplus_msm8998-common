e1f735f1e3969d9041310eea856acdd969795517
Timi <timi.rautamaki@gmail.com>
2020-11-11 13:00:02 +0100

    msm8998-common: update vendor security patch level
     * OxygenOS 10.0.1 updates to September patch level
    
    Change-Id: I7e1013801f72ffcd303902243503cf481d2eeda5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -212,7 +212,7 @@ BOARD_ROOT_EXTRA_SYMLINKS += /vendor/firmware_mnt:/firmware
 BOARD_ROOT_EXTRA_SYMLINKS += /mnt/vendor/persist:/persist
 
 # Security
-VENDOR_SECURITY_PATCH := 2020-04-05
+VENDOR_SECURITY_PATCH := 2020-09-01
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
