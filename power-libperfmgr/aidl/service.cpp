a342462cacda4e34ed262ecfafd270fb84f92b0e
Wei Wang <wvw@google.com>
2020-11-13 10:59:00 -0800

    msm8998-common: power: add a property for config file
    Test: build
    Bug: 173222103
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: I14e7e0aa349e446f6889cbfc9a914f5314438e6b

--- power-libperfmgr/aidl/service.cpp
+++ power-libperfmgr/aidl/service.cpp
@@ -32,16 +32,21 @@ using aidl::google::hardware::power::impl::pixel::Power;
 using aidl::google::hardware::power::impl::pixel::PowerExt;
 using ::android::perfmgr::HintManager;
 
-constexpr char kPowerHalConfigPath[] = "/vendor/etc/powerhint.json";
-constexpr char kPowerHalInitProp[] = "vendor.powerhal.init";
+constexpr std::string_view kPowerHalInitProp("vendor.powerhal.init");
+constexpr std::string_view kConfigProperty("vendor.powerhal.config");
+constexpr std::string_view kConfigDefaultFileName("powerhint.json");
 
 int main() {
-    LOG(INFO) << "Power HAL AIDL Service with Extension is starting.";
+    const std::string config_path =
+            "/vendor/etc/" +
+            android::base::GetProperty(kConfigProperty.data(), kConfigDefaultFileName.data());
+    LOG(INFO) << "Power HAL AIDL Service with Extension is starting with config: "
+              << config_path;
 
     // Parse config but do not start the looper
-    std::shared_ptr<HintManager> hm = HintManager::GetFromJSON(kPowerHalConfigPath, false);
+    std::shared_ptr<HintManager> hm = HintManager::GetFromJSON(config_path, false);
     if (!hm) {
-        LOG(FATAL) << "Invalid config: " << kPowerHalConfigPath;
+        LOG(FATAL) << "Invalid config: " << config_path;
     }
 
     std::shared_ptr<DisplayLowPower> dlpw = std::make_shared<DisplayLowPower>();
@@ -65,7 +70,7 @@ int main() {
     LOG(INFO) << "Power HAL AIDL Service started.";
 
     std::thread initThread([&]() {
-        ::android::base::WaitForProperty(kPowerHalInitProp, "1");
+        ::android::base::WaitForProperty(kPowerHalInitProp.data(), "1");
         hm->Start();
         dlpw->Init();
     });
