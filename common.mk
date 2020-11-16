18938a49fc4bccd1c5f3e4f592940278fbb6730e
Timi <timi.rautamaki@gmail.com>
2020-11-16 19:03:48 +0100

    msm8998-common: build libstagefright_enc_common for WFD
    Change-Id: Ib2df1b1f883212e7999afdbf146a8294efc1e62d

--- common.mk
+++ common.mk
@@ -535,7 +535,8 @@ PRODUCT_COPY_FILES += \
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
     libaacwrapper \
-    libnl
+    libnl \
+    libstagefright_enc_common
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
