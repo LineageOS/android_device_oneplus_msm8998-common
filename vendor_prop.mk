6087cd864943a5a293be3a15d883fda09151026c
Georg Veichtlbauer <georg@vware.at>
2020-12-23 07:47:10 +0100

    msm8998-common: remove usb vendor properties
    * set up happens in init
    
    Change-Id: Ic394de748bb54c6b870abf8af024b8759b548a07

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -297,14 +297,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.hardcoder.name=oiface \
     persist.sys.oiface.feature=oiface:0xF,oifaceim:ffffffff
 
-# USB
-PRODUCT_PROPERTY_OVERRIDES += \
-    sys.usb.controller=a800000.dwc3 \
-    sys.usb.rndis.func.name=gsi \
-    sys.usb.rmnet.func.name=gsi \
-    persist.vendor.usb.config.extra=none \
-    vendor.usb.rps_mask=0
-
 # VNDK
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.vndk.version=28
