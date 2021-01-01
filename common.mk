9f9b2e8133716bee32787aa9beed8abf71753e67
Michael Bestas <mkbestas@lineageos.org>
2021-01-01 04:57:09 +0200

    msm8998-common: Remove android.hardware.graphics.composer@2.1-impl
    It no longer exists, the service is fully binderized
    
    Change-Id: I06ee402b50216948e0e72a2797c2edfda1c4886a

--- common.mk
+++ common.mk
@@ -166,7 +166,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl:64 \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.configstore@1.0-service \
