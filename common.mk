91f09fae9bc3cb2c0f0f11a99d0e9a2cade30d25
Bruno Martins <bgcngm@gmail.com>
2021-01-03 11:17:21 +0000

    msm8998-common: Deduplicate handheld_core_hardware.xml copy rule
    Since Android 10 that this is already done while inheriting from
    full_base_telephony.mk.
    
    Change-Id: If533fbf95a9cf9798ddba24955a624d26f550d62

--- common.mk
+++ common.mk
@@ -74,8 +74,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
 # Haters gonna hate..
 PRODUCT_CHARACTERISTICS := nosdcard
