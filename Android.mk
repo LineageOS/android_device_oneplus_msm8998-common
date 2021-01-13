865a9806a35c2539fde74a5203b9415f36abd201
Pranav Vashi <neobuddy89@gmail.com>
2021-01-14 01:37:31 +0530

    msm8998-common: Move telephony packages to system_ext
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I5f6b0c9d64fbff4e04e7697b457a76328d42d6c0

--- Android.mk
+++ Android.mk
@@ -34,12 +34,12 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT)/priv-app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /product/lib64/$(notdir $@) $@
+	$(hide) ln -sf /system_ext/lib64/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
