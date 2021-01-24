74adebd279de46525739c6c39cdd83f13703ecd1
Jimmy Shiu <jimmyshiu@google.com>
2021-01-25 01:20:38 +0800

    msm8998-common: power: add PowerHintSession for ADPF implementation
    Adapted from PoC from ag/13100800
    
    Added more ATRACE for further tuning and debug
    
    Test: APPPID=$(adb shell pidof com.prefabulated.touchlatency); watch -n
          1 adb shell grep uclamp /proc/${APPPID}/sched
    Test: atest VtsHalPowerTargetTest
    Bug: 177492680
    Change-Id: I6bfd61b21dc1cde04f6ba9ae8d3533cd263ad814
    Signed-off-by: Wei Wang <wvw@google.com>

--- power-libperfmgr/disp-power/DisplayLowPower.cpp
+++ power-libperfmgr/disp-power/DisplayLowPower.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.power@-service.oneplus_msm8998-libperfmgr"
+#define LOG_TAG "powerhal-libperfmgr"
 
 #include <errno.h>
 #include <unistd.h>
