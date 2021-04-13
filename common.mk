20b6f6618bcb972cbe0ecefafb12a91eeafe3ee4
Jakub Pawlowski <jpawlowski@google.com>
2021-04-13 11:00:12 +0200

    msm8998-common: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development
    
    Bug: 150670922
    Change-Id: Iac2ece0c029a38fef82e81f5ba3ba491163818ef

--- common.mk
+++ common.mk
@@ -133,7 +133,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     audio.bluetooth.default \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
