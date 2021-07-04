aa36134a7137527df044df6a5873f4929abb925f
Marc Bourgoin <themard85@gmail.com>
2021-07-04 15:02:02 -0600

    msm8998-common: Disable WfdCommon boot jar
    Change-Id: I3dd591444dc54ebe3a32e0385cbfc3db6bfc5e1f

--- common.mk
+++ common.mk
@@ -484,9 +484,6 @@ PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Inherit from oppo-common
 $(call inherit-product, device/oppo/common/common.mk)
 
