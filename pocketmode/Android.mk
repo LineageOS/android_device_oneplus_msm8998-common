497e9beece044e26343e15454492f1d763f6d017
Georg Veichtlbauer <georg@vware.at>
2021-07-30 07:29:19 +0200

    msm8998-common: pocketmode: Cleanup
    - Remove unneeded stuff
    - Update package name and tag
    - Destroy class only after unregistering
    
    Change-Id: I47ad0090ff61a2448bff2364f059504f0bc4deb3

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -1,13 +1,14 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := OneplusPocketMode
-LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PACKAGE_NAME := OnePlusPocketMode
 LOCAL_CERTIFICATE := platform
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
