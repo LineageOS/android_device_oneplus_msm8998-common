3972b2341775e5eb39065da7892fb3d02af60f10
Georg Veichtlbauer <georg@vware.at>
2021-07-31 10:09:58 +0200

    msm8998-common: doze: Cleanup and rename package
    - Bring us in line with other Oneplus devices
    
    Change-Id: I414b3a3d32a48d25a826892b76996d361839df74

--- doze/Android.mk
+++ doze/Android.mk
@@ -1,11 +1,12 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := Doze
+LOCAL_PACKAGE_NAME := OnePlusDoze
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
@@ -24,5 +25,3 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
