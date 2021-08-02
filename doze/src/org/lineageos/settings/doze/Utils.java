9cb63bfc9e635cf6771bd39b5ebf06741fe5aeaa
Georg Veichtlbauer <georg@vware.at>
2021-08-02 07:30:54 +0200

    msm8998-common: doze: Order class member variables
    Change-Id: Iec03dd4e318d40cc23417051829e07638f14f2c3

--- doze/src/org/lineageos/settings/doze/Utils.java
+++ doze/src/org/lineageos/settings/doze/Utils.java
@@ -32,14 +32,16 @@ import static android.provider.Settings.Secure.DOZE_ENABLED;
 
 public final class Utils {
 
+    private static final boolean DEBUG = false;
+    private static final String TAG = "DozeUtils";
+
+    private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
+
     protected static final String WAKE_ON_GESTURE_KEY = "wake_on_gesture";
     protected static final String CATEG_PROX_SENSOR = "proximity_sensor";
     protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up";
     protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
     protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
-    private static final String TAG = "DozeUtils";
-    private static final boolean DEBUG = false;
-    private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
 
     protected static void startService(Context context) {
         if (DEBUG) Log.d(TAG, "Starting service");
