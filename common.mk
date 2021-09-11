6fd9a75c5611f936bbc6e5db2cbe54266f0e3062
Alexander Koskovich <akoskovich@pm.me>
2021-09-10 20:38:25 -0700

    msm8998-common: Remove vendor RenderScript implementation.
     * RenderScript is deprecated on newer platforms and
       is being officially replaced.
    
     * On April 19, 2021, Google announced that RenderScript
       will be deprecated in Android 12, and recommended
       porting existing code to Vulkan.
    
       https://android-developers.googleblog.com/2021/04/android-gpu-compute-going-forward.html
    
    Change-Id: I19460ef266a646b046f1e7d2f0b4eab7c48ae536

--- common.mk
+++ common.mk
@@ -370,10 +370,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     librecovery_updater_oneplus
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.1.vendor \
