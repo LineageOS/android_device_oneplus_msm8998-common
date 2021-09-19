94756662df09e4274d4f82b82a4230788d9421ee
LuK1337 <priv.luk@gmail.com>
2021-09-19 14:59:47 +0200

    msm8998-common: Switch to hardware/oneplus components
    Change-Id: Id46ce3e49fda2404c8936039d5a090dc33f956f8

--- common.mk
+++ common.mk
@@ -174,7 +174,7 @@ PRODUCT_PACKAGES += \
 
 # Doze
 PRODUCT_PACKAGES += \
-    OnePlusDoze
+    OnePlus5Doze
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -410,7 +410,8 @@ PRODUCT_SHIPPING_API_LEVEL := 25
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/google/pixel
+    hardware/google/pixel \
+    hardware/oneplus
 
 # Tetheroffload
 PRODUCT_PACKAGES += \
@@ -419,7 +420,12 @@ PRODUCT_PACKAGES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.oneplus_msm8998
+    vendor.lineage.touch@1.0-service.oneplus
+
+# tri-state-key
+PRODUCT_PACKAGES += \
+    KeyHandler \
+    tri-state-key_daemon.vendor
 
 # Trust HAL
 PRODUCT_PACKAGES += \
@@ -474,9 +480,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libaacwrapper \
     libnl
-
-# Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
