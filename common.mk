460bcafaf40ec8b37d042f98e1a8faeb9bcb62a2
Georg Veichtlbauer <georg@vware.at>
2021-09-29 08:19:13 +0200

    msm8998-common: Build hw/op KeyHandler
    Change-Id: Icf3852afc86351d7f347f87de846e3ec785c8935

--- common.mk
+++ common.mk
@@ -414,6 +414,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# Tri-state-key
+PRODUCT_PACKAGES += \
+    KeyHandler
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
