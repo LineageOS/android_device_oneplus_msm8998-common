c2817a2741ab4a5163b798feb1e656adaf9f2197
Georg Veichtlbauer <georg@vware.at>
2021-10-05 18:00:17 +0200

    msm8998-common: Build and use audio_amplifier
    * From hardware/oneplus
    
    Change-Id: I78ad5d0c6de174051b88f6fdaa93e568e68697b8

--- common.mk
+++ common.mk
@@ -99,6 +99,7 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8998 \
     audio.r_submix.default \
     audio.usb.default \
+    audio_amplifier.msm8998 \
     libaudio-resampler \
     libqcompostprocbundle \
     libqcomvisualizer \
