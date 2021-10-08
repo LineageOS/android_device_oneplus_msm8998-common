94e244e759ada7c53fc33b4c74dfae8ec1212202
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-10-08 07:59:14 +0000

    msm8998-common: update system/core/base -> system/libbase
    Change-Id: I1cd3855fe871155841660ca93f200e9c0149ccc3

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -11,7 +11,7 @@ LOCAL_C_INCLUDES := \
     bootable/recovery/edify/include \
     bootable/recovery/otautil/include \
     bootable/recovery/updater/include \
-    system/core/base/include
+    system/libbase/include
 
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_oneplus
