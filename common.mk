e7c6cee150b16a42f6342f47c79c875805785fe0
Georg Veichtlbauer <georg@vware.at>
2021-10-09 08:27:00 +0200

    msm8998-common: init: Convert file copying to build targets
    Change-Id: I4099c1dbde80d4538b44e3c2b6e0ded40132fd0f

--- common.mk
+++ common.mk
@@ -235,13 +235,13 @@ PRODUCT_PACKAGES += \
     libhwbinder.vendor
 
 # Init
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/init.devstart.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.devstart.sh \
-    $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
-    $(LOCAL_PATH)/rootdir/etc/init.qcom.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.usb.rc \
-    $(LOCAL_PATH)/rootdir/etc/init.radio.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.radio.sh \
-    $(LOCAL_PATH)/rootdir/etc/ueventd.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.devstart.sh \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.radio.sh \
+    ueventd.rc
 
 # IFAA (Fingerprint support for Alipay)
 PRODUCT_PACKAGES += \
