46b960c3e46b065fe33f222fd5b8c9bf8b173c27
Georg Veichtlbauer <georg@vware.at>
2021-10-29 12:22:16 +0200

    msm8998-common: Switch to jasmine Bluetooth/ANT
    * Move ANT+ to /system_ext
    * Also, ANT+ is not Bluetooth
    
    Change-Id: I76481bd6d9b7de76b0b415737cc587efd7ddbfdc

--- common.mk
+++ common.mk
@@ -132,7 +132,8 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl:32 \
-    audio.bluetooth.default
+    audio.bluetooth.default \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
