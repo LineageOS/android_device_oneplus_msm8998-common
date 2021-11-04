0d7c97ef445f2644ac862a9c915b5d756ef0a02f
Georg Veichtlbauer <georg@vware.at>
2021-11-05 00:21:04 +0100

    msm8998-common: Rewrite light HAL implementation
    Inspired by fellow OnePlus devices, but:
     - Add button backlight controls
     - Ramp duration altered
     - Keep our improved brightness scaling
     - Cleanup and update in service startup
    
    This brings some notable improvements compared to the old one:
     - Global lock centrally acquired
     - Better LED ramps
     - Notifications take priority now
     - Reduced logspam
     - Much improved memory footprint
    
    Change-Id: I33d25cd1ee33118f9f754cbfffe2ebd45483ac87

--- light/service.cpp
+++ light/service.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The LineageOS Project
+ * Copyright (C) 2018-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,91 +18,27 @@
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
-#include <utils/Errors.h>
 
 #include "Light.h"
 
-// libhwbinder:
-using android::hardware::configureRpcThreadpool;
-using android::hardware::joinRpcThreadpool;
-
 // Generated HIDL files
 using android::hardware::light::V2_0::ILight;
-using android::hardware::light::V2_0::implementation::Led;
 using android::hardware::light::V2_0::implementation::Light;
 
-const static std::string kLcdBacklightPath = "/sys/class/leds/lcd-backlight/brightness";
-const static std::string kLcdMaxBacklightPath = "/sys/class/leds/lcd-backlight/max_brightness";
-const static std::string kButtonBacklightPath = "/sys/class/leds/button-backlight/brightness";
-const static std::string kRgbBlinkPath = "/sys/class/leds/rgb/rgb_blink";
-
 int main() {
-    uint32_t lcdMaxBrightness = 255;
+    android::sp<ILight> service = new Light();
 
-    std::ofstream lcdBacklight(kLcdBacklightPath);
-    if (!lcdBacklight) {
-        LOG(ERROR) << "Failed to open " << kLcdBacklightPath << ", error=" << errno
-                   << " (" << strerror(errno) << ")";
-        return -errno;
-    }
+    android::hardware::configureRpcThreadpool(1 /*threads*/, true /*callerWillJoin*/);
 
-    std::ifstream lcdMaxBacklight(kLcdMaxBacklightPath);
-    if (!lcdMaxBacklight) {
-        LOG(ERROR) << "Failed to open " << kLcdMaxBacklightPath << ", error=" << errno
-                   << " (" << strerror(errno) << ")";
-        return -errno;
-    } else {
-        lcdMaxBacklight >> lcdMaxBrightness;
-    }
-
-    std::ofstream buttonBacklight(kButtonBacklightPath);
-    if (!buttonBacklight) {
-        LOG(WARNING) << "Failed to open " << kButtonBacklightPath << ", error=" << errno
-                     << " (" << strerror(errno) << ")";
-    }
-
-    Led redLed(0, "red");
-    if (!redLed) {
-        LOG(ERROR) << "Failed to create red LED";
-        return -1;
-    }
-
-    Led greenLed(1, "green");
-    if (!greenLed) {
-        LOG(ERROR) << "Failed to create green LED";
-        return -1;
-    }
-
-    Led blueLed(2, "blue");
-    if (!blueLed) {
-        LOG(ERROR) << "Failed to create blue LED";
-        return -1;
-    }
-
-    std::ofstream rgbBlink(kRgbBlinkPath);
-    if (!rgbBlink) {
-        LOG(ERROR) << "Failed to open " << kRgbBlinkPath << ", error=" << errno
-                   << " (" << strerror(errno) << ")";
-        return -errno;
-    }
-
-    android::sp<ILight> service = new Light(
-            { std::move(lcdBacklight), lcdMaxBrightness },
-            std::move(buttonBacklight), std::move(redLed), std::move(greenLed),
-            std::move(blueLed), std::move(rgbBlink));
-
-    configureRpcThreadpool(1, true);
-
-    android::status_t status = service->registerAsService();
-
-    if (status != android::OK) {
-        LOG(ERROR) << "Cannot register Light HAL service";
+    if (service->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register Light HAL service.";
         return 1;
     }
 
-    LOG(INFO) << "Light HAL Ready.";
-    joinRpcThreadpool();
-    // Under normal cases, execution will not reach this line.
+    LOG(INFO) << "Light HAL ready.";
+
+    android::hardware::joinRpcThreadpool();
+
     LOG(ERROR) << "Light HAL failed to join thread pool.";
     return 1;
 }
