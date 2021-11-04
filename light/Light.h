c777bf74328dccbc32908544c11e48c0cf1bcc6d
Georg Veichtlbauer <georg@vware.at>
2021-11-05 00:21:04 +0100

    msm8998-common: Rewrite light HAL implementation
    Inspired by fellow OnePlus devices, but:
     - Add button backlight controls
     - Keep our improved brightness scaling
     - Cleanup and update in service startup
    
    This brings some notable improvements compared to the old one:
     - Global lock centrally acquired
     - Better LED ramps
     - Notifications take priority now
     - Reduced logspam
     - Much improved memory footprint
    
    Change-Id: I33d25cd1ee33118f9f754cbfffe2ebd45483ac87

--- light/Light.h
+++ light/Light.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The LineageOS Project
+ * Copyright (C) 2018-2021 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,8 +19,6 @@
 
 #include <android/hardware/light/2.0/ILight.h>
 #include <hidl/Status.h>
-
-#include <fstream>
 #include <mutex>
 #include <unordered_map>
 
@@ -30,66 +28,30 @@ namespace light {
 namespace V2_0 {
 namespace implementation {
 
-class Led {
-private:
-    Led(const Led&) = delete;
-    Led& operator=(const Led&) = delete;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::light::V2_0::ILight;
+using ::android::hardware::light::V2_0::LightState;
+using ::android::hardware::light::V2_0::Status;
+using ::android::hardware::light::V2_0::Type;
 
-public:
-    Led(int index, const std::string& name);
-    Led(Led&&) = default;
-    virtual ~Led() {}
-
-    explicit operator bool() const;
-    bool operator!() const;
-
-    void off() { setBrightness(0); }
-    void setBrightness(int value);
-    void setBlink(int brightness, int onMs, int offMs);
-
-private:
-    int                 mIndex;
-    std::string         mName;
-    std::ofstream       mBrightness;
-    std::ofstream       mDutyPcts;
-    std::ofstream       mStartIdx;
-    std::ofstream       mPauseLo;
-    std::ofstream       mPauseHi;
-    std::ofstream       mRampStepMs;
-    std::ofstream       mBlink;
-};
-
-struct Light : public ILight {
-    Light(std::pair<std::ofstream, uint32_t>&& lcd_backlight,
-          std::ofstream&& button_backlight, Led&& red_led, Led&& green_led,
-          Led&& blue_led, std::ofstream&& rgb_blink);
+class Light : public ILight {
+  public:
+    Light();
 
     // Methods from ::android::hardware::light::V2_0::ILight follow.
     Return<Status> setLight(Type type, const LightState& state) override;
     Return<void> getSupportedTypes(getSupportedTypes_cb _hidl_cb) override;
 
   private:
-    void setAttentionLight(const LightState& state);
-    void setBatteryLight(const LightState& state);
+    void setBacklight(const LightState& state);
     void setButtonBacklight(const LightState& state);
-    void setLcdBacklight(const LightState& state);
-    void setNotificationLight(const LightState& state);
-    void setSpeakerBatteryLightLocked();
-    void setSpeakerLightLocked(const LightState& state);
+    void setRgbLight(const LightState& state, size_t index);
 
-    std::pair<std::ofstream, uint32_t> mLcdBacklight;
-    std::ofstream&& mButtonBacklight;
-    Led mRedLed;
-    Led mGreenLed;
-    Led mBlueLed;
-    std::ofstream mRgbBlink;
-
-    LightState mAttentionState;
-    LightState mBatteryState;
-    LightState mNotificationState;
-
-    std::unordered_map<Type, std::function<void(const LightState&)>> mLights;
     std::mutex mLock;
+    std::unordered_map<Type, std::function<void(const LightState&)>> mLights;
+    std::array<LightState, 3> mLightStates;
 };
 
 }  // namespace implementation
