trisfaizal/device_xiaomi_msm8998-common-1

msm8998-common: power: display: cache display lpm state to avoid set it again
If current foss state same as desired state, doesn't set it again.

Bug: 148656663
Test: stress on/off battery saver and check foss state
Test: enable battery saver, stress on/off screen
Change-Id: I6298361fd6a5d6a86c94eabb71ab254507136fd3
Signed-off-by: 0ranko0P <ranko0p@outlook.com>

power-libperfmgr/disp-power/DisplayLowPower.cpp
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "android.hardware.power@-service.pixel-libperfmgr"
+
+#include <errno.h>
+#include <unistd.h>
+
+#include <cutils/sockets.h>
+#include <log/log.h>
+
+#include "DisplayLowPower.h"
+
+DisplayLowPower::DisplayLowPower() : mFossStatus(false) {}
+
+void DisplayLowPower::Init() {
+    ConnectPpsDaemon();
+}
+
+void DisplayLowPower::SetDisplayLowPower(bool enable) {
+    SetFoss(enable);
+}
+
+void DisplayLowPower::ConnectPpsDaemon() {
+    constexpr const char kPpsDaemon[] = "pps";
+
+    mPpsSocket.reset(
+            socket_local_client(kPpsDaemon, ANDROID_SOCKET_NAMESPACE_RESERVED, SOCK_STREAM));
+    if (mPpsSocket.get() < 0) {
+        ALOGW("Connecting to PPS daemon failed (%s)", strerror(errno));
+    }
+}
+
+int DisplayLowPower::SendPpsCommand(const std::string_view cmd) {
+    if (TEMP_FAILURE_RETRY(write(mPpsSocket.get(), cmd.data(), cmd.size())) < 0) {
+        ALOGE("Failed to send pps command '%s' over socket (%s)", cmd.data(), strerror(errno));
+        return -1;
+    }
+
+    return 0;
+}
+
+void DisplayLowPower::SetFoss(bool enable) {
+    if (mPpsSocket.get() < 0 || mFossStatus == enable) {
+        return;
+    }
+
+    ALOGI("%s foss", (enable) ? "Enable" : "Disable");
+
+    std::string_view foss_cmd;
+    if (enable) {
+        foss_cmd = "foss:on";
+    } else {
+        foss_cmd = "foss:off";
+    }
+
+    if (!SendPpsCommand(foss_cmd)) {
+        mFossStatus = enable;
+    }
+}

LineageOS/android_device_oneplus_msm8998-common

power-libperfmgr/disp-power/DisplayLowPower.cpp
@@ -1,0 +1,73 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "android.hardware.power@-service.oneplus_msm8998-libperfmgr"
+
+#include <errno.h>
+#include <unistd.h>
+
+#include <cutils/sockets.h>
+#include <log/log.h>
+
+#include "DisplayLowPower.h"
+
+DisplayLowPower::DisplayLowPower() : mFossStatus(false) {}
+
+void DisplayLowPower::Init() {
+    ConnectPpsDaemon();
+}
+
+void DisplayLowPower::SetDisplayLowPower(bool enable) {
+    SetFoss(enable);
+}
+
+void DisplayLowPower::ConnectPpsDaemon() {
+    constexpr const char kPpsDaemon[] = "pps";
+
+    mPpsSocket.reset(
+            socket_local_client(kPpsDaemon, ANDROID_SOCKET_NAMESPACE_RESERVED, SOCK_STREAM));
+    if (mPpsSocket.get() < 0) {
+        ALOGW("Connecting to PPS daemon failed (%s)", strerror(errno));
+    }
+}
+
+int DisplayLowPower::SendPpsCommand(const std::string_view cmd) {
+    if (TEMP_FAILURE_RETRY(write(mPpsSocket.get(), cmd.data(), cmd.size())) < 0) {
+        ALOGE("Failed to send pps command '%s' over socket (%s)", cmd.data(), strerror(errno));
+        return -1;
+    }
+
+    return 0;
+}
+
+void DisplayLowPower::SetFoss(bool enable) {
+    if (mPpsSocket.get() < 0 || mFossStatus == enable) {
+        return;
+    }
+
+    ALOGI("%s foss", (enable) ? "Enable" : "Disable");
+
+    std::string_view foss_cmd;
+    if (enable) {
+        foss_cmd = "foss:on";
+    } else {
+        foss_cmd = "foss:off";
+    }
+
+    if (!SendPpsCommand(foss_cmd)) {
+        mFossStatus = enable;
+    }
+}