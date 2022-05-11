8918ccc59899636b09f941710ef5e74d0d309c15
Georg Veichtlbauer <georg@vware.at>
2022-05-11 12:32:41 +0200

    msm8998-common: Don't specify unnecessary vendor lib
    * hardware.drm@1.1 is built as dependency
    
    Change-Id: Ibebf6326c762ed2e2cca2df914c98b6e83c4c7f8

--- common.mk
+++ common.mk
@@ -180,7 +180,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.4-service.clearkey
 
