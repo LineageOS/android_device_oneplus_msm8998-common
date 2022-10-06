8e2733e3048de6e8de7951e3020cb435b7d5c7b1
Georg Veichtlbauer <georg@vware.at>
2022-10-06 14:42:49 +0200

    msm8998-common: Patch lib-imsvideocodec
    * missing symbol GraphicBufferProducer in libgui_shim.so
    * replace libqdMetaData.so with libqdMetaData.system.so
    
    Change-Id: I41b8804d84b15caa41ec2c7298bcce02c003bb53

--- extract-files.sh
+++ extract-files.sh
@@ -69,6 +69,10 @@ function blob_fixup() {
         system_ext/etc/permissions/com.qti.dpmframework.xml | system_ext/etc/permissions/dpmapi.xml)
             sed -i "s|/product/framework/|/system_ext/framework/|g" "${2}"
             ;;
+        system_ext/lib64/lib-imsvideocodec.so)
+            "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+            "${PATCHELF}" --replace-needed "libqdMetaData.so" "libqdMetaData.system.so" "${2}"
+            ;;
         system_ext/lib64/libdpmframework.so )
             sed -i "s|libhidltransport.so|libcutils-v29.so\x00\x00\x00|" "${2}"
             ;;
