b0286b74c199111c1b7c5ae1a3331e4ef089abb9
Georg Veichtlbauer <georg@vware.at>
2022-10-09 11:24:08 +0200

    msm8998-common: Clean up listen / soundtrigger
    * Get rid of "Misc" section and integrate files where they belong
    * Pull graphite_ipc_platform_info into device tree
    * Remove unused 64bit libraries
    
    Change-Id: I6441924c49c7e6491218fbab9dffd38cf1c58237

--- common.mk
+++ common.mk
@@ -122,6 +122,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/audio/graphite_ipc_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/graphite_ipc_platform_info.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
