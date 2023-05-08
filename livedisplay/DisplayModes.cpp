2f78d814f76df7a27c4ae53a4f1e704f9dce2893
Georg Veichtlbauer <georg@vware.at>
2023-05-08 12:46:10 +0200

    msm8998-common: livedisplay: Re-introduce Standard display mode
    After switching to the in-kernel LiveDisplay driver, getting the
    display into "standard" mode means switching off whatever mode was
    previously selected.
    Thus, the kernel now contains new "presets" 4-7 which will switch
    off their corresponding color presets of 0-3.
    
    Change-Id: Ic880c5160eea8694d778a86274261f254f47fc54

--- livedisplay/DisplayModes.cpp
+++ livedisplay/DisplayModes.cpp
@@ -22,10 +22,11 @@ static const std::string kAvailableModesPath = "/sys/devices/virtual/graphics/fb
 static const std::string kDefaultPath = "/data/vendor/display/default_display_mode";
 
 const std::map<int32_t, std::string> DisplayModes::kModeMap = {
-    {0, "sRGB"},
-    {1, "DCI-P3"},
-    {2, "OnePlus"},
-    {3, "Adaptive"},
+    {0, "Standard"},
+    {1, "sRGB"},
+    {2, "DCI-P3"},
+    {3, "OnePlus"},
+    {4, "Adaptive"},
 };
 
 DisplayModes::DisplayModes()
@@ -51,7 +52,7 @@ Return<void> DisplayModes::getDisplayModes(getDisplayModes_cb resultCb) {
     if (maxModeCount > 0) {
         for (const auto& entry : kModeMap) {
             modes.push_back({entry.first, entry.second});
-            if (entry.first == maxModeCount - 1)
+            if (entry.first == maxModeCount - 4)
                 break;
         }
     }
@@ -75,7 +76,12 @@ Return<bool> DisplayModes::setDisplayMode(int32_t modeID, bool makeDefault) {
         return false;
     }
     std::ofstream file(kModeBasePath);
-    file << modeID;
+    // If mode is "Standard" we need to switch off the previously selected mode
+    // Modes 0-3 are colors (0=sRGB, etc.), 4-7 to switch them off (4=sRGB off, etc.)
+    if (modeID == 0)
+        file << mDefaultModeId + 3;
+    else
+        file << modeID - 1;
     if (file.fail()) {
         LOG(ERROR) << "Failed to write to " << (kModeBasePath);
     }
