45e43b45b871f528d8633afab7ee5b3c70802e97
LuK1337 <priv.luk@gmail.com>
2023-10-31 19:06:44 +0100

    msm8998-common: Get rid of AVB 1.0
    It's dead in U.
    
    Change-Id: Id948fe50fd358837cafa3b1e18cf2adba32b927b

--- common.mk
+++ common.mk
@@ -399,10 +399,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
-# Verity
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
-$(call inherit-product, build/target/product/verity.mk)
-
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
