f210593b14c16f6ae0462d29a06eb3b75788c1cb
Georg Veichtlbauer <georg@vware.at>
2023-11-09 14:55:02 +0100

    msm8998-common: update-sha1sums: Use Python 3
    Change-Id: I79d6eeea5dc6dc570e72150379befd205a6cb72d

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python3
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
