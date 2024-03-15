6d37d5cfa71bfc95b1b3e0f39a9897e37f9ab55d
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-15 15:17:25 +0800

    Revert "msm8998-common: Switch to legacy WiFi HIDL HAL"
    * WiFi AIDL HAL works in U QPR2
    
    This reverts commit 7631e8012c5b5e09d28a4ec3d81980434d0b28f8.
    
    Change-Id: I4d38fcc7e5999f934344bc364afb4f4d8a049711

--- common.mk
+++ common.mk
@@ -425,7 +425,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy \
+    android.hardware.wifi-service \
     hostapd \
     hostapd_cli \
     libwifi-hal-qcom \
