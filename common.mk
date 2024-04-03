9f67e3f8476d10cebf2004b26e74b267d8df39a4
Georg Veichtlbauer <georg@vware.at>
2024-04-03 23:24:33 +0200

    msm8998-common: Switch blob source to FP3
    Change-Id: Ic23eb57d1f0b6d89c36ac3f4a4ea8d6bfcf7dd56

--- common.mk
+++ common.mk
@@ -122,6 +122,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.4:64 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.6:64 \
     libstdc++_vendor \
     vendor.qti.hardware.camera.device@1.0
 
