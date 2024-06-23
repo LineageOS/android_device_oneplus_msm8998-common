2783373e8342e44eede5e1b579180df5e67d3809
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:39:38 +0100

    msm8998-common: Unset BUILD_BROKEN_INCORRECT_PARTITION_IMAGES
    Qualcomm targets can now build hermetic partitions.
    
    Change-Id: I241105e01d85fea983b34846ca675666ac07541b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,7 +32,6 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
 
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
