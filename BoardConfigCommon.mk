221ad1d5d7876c8c1710fa383fa0028783d88ee9
Aaron Kling <webgeek1234@gmail.com>
2024-12-10 22:46:54 +0100

    msm8998-common: remove elf files in copy files check
    New extract files uses packages instead of copy files
    
    Change-Id: I337c07d13bd350958d4808e1693cc6e03753a96c
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,7 +31,6 @@ TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
 
 BUILD_BROKEN_DUP_RULES := true
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3
