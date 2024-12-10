112c740a6d40d42df32ef0c133ceabbd4ecb926a
Aaron Kling <webgeek1234@gmail.com>
2024-12-10 22:47:36 +0100

    msm8998-common: re-enable broken dup rules check
    All failures of this have been resolved
    
    Change-Id: I56a923ac90b98e63c093c8cb65cb1059dde57c49
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,8 +30,6 @@ TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
 
-BUILD_BROKEN_DUP_RULES := true
-
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048
