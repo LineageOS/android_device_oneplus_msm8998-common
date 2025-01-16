fca09ac275042f39464eaf4d34581f8d18d35532
Georg Veichtlbauer <georg@vware.at>
2025-01-16 13:31:33 +0100

    msm8998-common: rootdir: Move sh files to bin
    Change-Id: Ic44e8b23e4e3565e9246d950703ee6ac6e7a9307

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -12,7 +12,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.devstart.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.devstart.sh
+LOCAL_SRC_FILES    := bin/init.devstart.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
@@ -36,7 +36,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.radio.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.radio.sh
+LOCAL_SRC_FILES    := bin/init.radio.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
