77647117deaaa8de0b685a985d39cea361f6e612
Michael Bestas <mkbestas@lineageos.org>
2025-03-23 18:44:57 +0100

    msm8998-common: Add missing lib-rcsconfig.so
    Change-Id: Ic2e503e98da4de63d76620772dd86bd2f2b5c87b

--- extract-files.py
+++ extract-files.py
@@ -41,7 +41,6 @@ lib_fixups: lib_fixups_user_type = {
         'libarcsoft_smart_denoise',
         'libarcsoft_high_dynamic_range',
         'libwpa_client',
-        'lib-rcsconfig',
     ): lib_fixup_remove,
 }
 
