690775bcad6b5c410cc044a70593a07764e64eb8
Roberto Sartori <roberto.sartori.android@gmail.com>
2025-03-26 22:24:47 +0100

    msm8998-common: Enable ELF checks for libEIS
    Added dependency: libsensor1.so
    
    Change-Id: I4da853f10d38df9d0d1cbd1471927b0c91e786f0

--- extract-files.py
+++ extract-files.py
@@ -75,6 +75,11 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('__aeabi_memcpy')
         .clear_symbol_version('__aeabi_memset')
         .clear_symbol_version('__gnu_Unwind_Find_exidx'),
+    (
+        'vendor/lib/libEIS.so',
+        'vendor/lib64/libEIS.so',
+    ): blob_fixup()
+        .add_needed('libsensor1.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
