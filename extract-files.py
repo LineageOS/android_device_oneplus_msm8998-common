9c512cbd6a3c94d19683cf4e0adb45c45e1dca71
SirRGB <sirrgb@proton.me>
2025-03-30 14:57:58 +0200

    msm8998-common: Enable ELF checks for libarcsoft_hdr_denoise_api
    Added missing blobs:
    libarcsoft_high_dynamic_range.so
    libarcsoft_smart_denoise.so
    
    Change-Id: If8e934ca4a89301ac090116380d3518b0e4aec34

--- extract-files.py
+++ extract-files.py
@@ -38,8 +38,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
     (
-        'libarcsoft_smart_denoise',
-        'libarcsoft_high_dynamic_range',
     ): lib_fixup_remove,
 }
 
