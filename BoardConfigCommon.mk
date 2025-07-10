92b577f5dd8d4a0b8f96fca6e8ce5a54602bdcc5
LuK1337 <priv.luk@gmail.com>
2025-07-10 09:44:13 +0000

    msm8998-common: Migrate to soong_config_set()
    Change-Id: I06de8759836114f54a822d6f23f1177515e21178

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,8 +6,6 @@
 
 PLATFORM_PATH := device/oneplus/msm8998-common
 
-TARGET_SPECIFIC_HEADER_PATH := $(PLATFORM_PATH)/include
-
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := msm8998
 TARGET_NO_BOOTLOADER := true
